0fd4dd8478e8d33e699375ca3aa13e0cc1d0892e
Tai Sakuma <tai.sakuma@gmail.com>
2020-10-19 16:52:21 -0400

    stop supporting Python 3.6 and 3.7
    These versions don't have AsyncMock
    https://travis-ci.com/github/simonsobs/nextline/builds/191042774

--- setup.py
+++ setup.py
@@ -20,8 +20,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
