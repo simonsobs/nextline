d14518cdce392dcb18fddbec7281840935bf7846
Tai Sakuma <tai.sakuma@gmail.com>
2020-10-24 13:53:40 -0400

    replace a dict local_controls with two sets

--- nextline/control.py
+++ nextline/control.py
@@ -17,7 +17,8 @@ class Control:
         self.global_queue = global_queue
         self.local_queue_dict = local_queue_dict # shared
         self.condition = condition # used to lock for local_queue_dict
-        self.local_controls = {}
+        self.thread_task_ids = set()
+        self.local_control_tasks = set()
 
     async def run(self):
         await self._start_local_controls()
@@ -28,9 +29,11 @@ class Control:
             thread_task_id = await self.global_queue.async_q.get()
             if thread_task_id is None: # end
                 break
-            if thread_task_id not in self.local_controls:
+            if thread_task_id not in self.thread_task_ids:
                 local_control = self._create_local_control(thread_task_id)
-                self.local_controls[thread_task_id] = asyncio.create_task(local_control.run())
+                self.thread_task_ids.add(thread_task_id)
+                task = asyncio.create_task(local_control.run())
+                self.local_control_tasks.add(task)
 
     def _create_local_control(self, thread_task_id):
         with self.condition:
@@ -45,7 +48,7 @@ class Control:
     async def _end(self):
         for _, q_out in self.local_queue_dict.values():
             q_out.sync_q.put(None)
-        await asyncio.gather(*self.local_controls.values())
+        await asyncio.gather(*self.local_control_tasks)
         self.global_queue.close()
         await self.global_queue.wait_closed()
 
