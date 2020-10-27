0dc34905d0f0692cef562a7d57e4a3bfeecde141
Tai Sakuma <tai.sakuma@gmail.com>
2020-10-27 11:42:45 -0400

    rename global_queue queue_trace_to_control

--- tests/test_control.py
+++ tests/test_control.py
@@ -8,20 +8,20 @@ from unittest.mock import Mock
 from nextline.control import Control
 
 ##__________________________________________________________________||
-async def communicate(global_queue):
+async def communicate(queue_trace_to_control):
     await asyncio.sleep(0.01)
     key = 'key'
-    await global_queue.async_q.put(key)
-    await global_queue.async_q.put(None)
+    await queue_trace_to_control.async_q.put(key)
+    await queue_trace_to_control.async_q.put(None)
 
 @pytest.mark.asyncio
 async def test_simple():
-    global_queue = janus.Queue()
+    queue_trace_to_control = janus.Queue()
     local_queue_dict = { }
     condition = threading.Condition()
-    control = Control(global_queue, local_queue_dict, condition)
+    control = Control(queue_trace_to_control, local_queue_dict, condition)
     control.run()
-    com = communicate(global_queue)
+    com = communicate(queue_trace_to_control)
     await asyncio.gather(control.wait(), com)
     assert {'key'} == control.thread_task_ids
     assert 1 == len(control.local_control_tasks)
