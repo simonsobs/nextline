75640a20842c1c831430aed2b38ad4054c8636ae
Tai Sakuma <tai.sakuma@gmail.com>
2020-11-11 20:07:02 -0500

    update tests for Trace

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -1,52 +1,56 @@
 import sys
-import queue
-
-from nextline.trace import Trace, LocalTrace, create_thread_task_id
 
 import pytest
-from unittest.mock import Mock
+from unittest.mock import Mock, call, sentinel
 
-##__________________________________________________________________||
-def subject():
-    return
+from nextline.trace import Trace, compose_thread_asynctask_id
+from nextline.pdb.proxy import PdbProxy
 
 ##__________________________________________________________________||
 @pytest.fixture()
-def mock_queue(monkeypatch):
-    y = Mock()
-    monkeypatch.setattr("nextline.trace.queue", y)
+def MockPdbProxy(monkeypatch):
+    y = Mock(spec=PdbProxy)
+    monkeypatch.setattr('nextline.trace.PdbProxy', y)
     yield y
 
-def test_trace(mock_queue):
+##__________________________________________________________________||
+def f():
+    pass
 
-    mock_queue.Queue().get.return_value = 'next'
+def subject():
+    f()
+    return
 
-    queue_to_control = Mock()
-    queue_from_control = Mock()
-    breaks = { __name__: ['subject'] }
-    trace = Trace(queue_to_control, queue_from_control, breaks)
+def test_sys_settrace(MockPdbProxy, snapshot):
+    """test with actual sys.settrace()
+    """
+    trace = Trace()
 
     trace_org = sys.gettrace()
     sys.settrace(trace)
     subject()
     sys.settrace(trace_org)
 
-    assert 3 == len(mock_queue.Queue().put.call_args_list)
-    assert 1 == len(queue_to_control.put.call_args_list)
+    id_ = compose_thread_asynctask_id()
+
+    assert [call(thread_asynctask_id=id_, trace=trace, state=trace.state)] == MockPdbProxy.call_args_list
+    assert 1 == MockPdbProxy().trace_func_init.call_count
+    assert 1 == MockPdbProxy().trace_func.call_count
 
 ##__________________________________________________________________||
-def test_local_trace():
-    local_queues = (Mock(), Mock())
-    q_in, q_out = local_queues
-    q_in.get.return_value = 'next'
+def test_return(MockPdbProxy):
+    """test if correct trace functions are returned
+    """
+    trace = Trace()
+    assert trace(sentinel.frame, 'call', None) is MockPdbProxy().trace_func_init()
+    assert trace(sentinel.frame, 'line', None) is MockPdbProxy().trace_func()
 
-    local_trace = LocalTrace(local_queues)
-
-    trace_org = sys.gettrace()
-    sys.settrace(local_trace)
-    subject()
-    sys.settrace(trace_org)
-
-    assert 3 == len(q_out.put.call_args_list)
+def test_args(MockPdbProxy, snapshot):
+    """test if arguments are properly propagated to the proxy
+    """
+    trace = Trace()
+    trace(sentinel.frame, 'call', None)
+    trace(sentinel.frame, 'line', None)
+    snapshot.assert_match(MockPdbProxy().method_calls)
 
 ##__________________________________________________________________||
