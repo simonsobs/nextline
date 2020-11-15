b2072b10d0872e43e01838abf05aa4af4dac36bf
Tai Sakuma <tai.sakuma@gmail.com>
2020-11-15 11:21:14 -0500

    store dict of thread_asynctask_id and pdb_ci

--- nextline/trace.py
+++ nextline/trace.py
@@ -104,15 +104,22 @@ class PdbCIRegistry:
     """
     def __init__(self):
         self.pdb_cis = []
+        self._dict = {}
         self.condition = threading.Condition()
 
-    def add(self, pdb_ci):
+    def add(self, thread_asynctask_id, pdb_ci):
         with self.condition:
+            self._dict[thread_asynctask_id] = pdb_ci
             self.pdb_cis.append(pdb_ci)
 
-    def remove(self, pdb_ci):
+    def remove(self, thread_asynctask_id):
         with self.condition:
+            pdb_ci = self._dict.pop(thread_asynctask_id)
             self.pdb_cis.remove(pdb_ci)
+
+    def get_ci(self, thread_asynctask_id):
+        with self.condition:
+            return self._dict.get(thread_asynctask_id, None)
 
 class Trace:
     """The main trace function
