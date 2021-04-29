c83fc62171829e772f27cd24cf1f192d42aee98d
Tai Sakuma <tai.sakuma@gmail.com>
2021-04-29 16:51:52 -0400

    update test_simple.py

--- tests/scenarios/test_simple.py
+++ tests/scenarios/test_simple.py
@@ -19,21 +19,33 @@ breaks = {
 @pytest.mark.asyncio
 async def test_run():
     nextline = Nextline(statement, breaks)
+
     global_state_subscription = nextline.subscribe_global_state()
     global_state = await global_state_subscription.__anext__()
     assert global_state == 'initialized'
+
     assert nextline.global_state == 'initialized'
+
     nextline.run()
+
     g = nextline.nextline_generator()
     nextline = await g.__anext__()
+
     time.sleep(0.02) # wait because sometimes pdb_ci is not in the registry yet
+
     global_state = await global_state_subscription.__anext__()
     assert global_state == 'running'
+
     assert nextline.global_state == 'running'
-    state = nextline.state.data
-    thread_id = list(state.keys())[0]
-    task_id = list(state[thread_id].keys())[0]
-    pdb_ci = nextline.pdb_ci_registry.get_ci((thread_id, task_id))
+
+    thread_asynctask_ids_subscription = nextline.subscribe_thread_asynctask_ids()
+    thread_asynctask_ids = await thread_asynctask_ids_subscription.__anext__()
+    thread_asynctask_id = thread_asynctask_ids[0]
+
+    thread_asynctask_state_subscription = nextline.subscribe_thread_asynctask_state(thread_asynctask_id)
+    thread_asynctask_state = await thread_asynctask_state_subscription.__anext__()
+
+    pdb_ci = nextline.pdb_ci_registry.get_ci(thread_asynctask_id)
     pdb_ci.send_pdb_command('continue')
     await nextline.wait()
     assert nextline.global_state == 'finished'
