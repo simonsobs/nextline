d9284ce3b089ca39f2e42e23b307dc4e5e37d163
Tai Sakuma <tai.sakuma@gmail.com>
2021-05-05 19:29:43 -0400

    update test for QueueDist

--- tests/test_queuedist.py
+++ tests/test_queuedist.py
@@ -1,9 +1,12 @@
 import asyncio
+import threading
+from functools import partial
 import time
 
 import pytest
 
 from nextline.queuedist import QueueDist
+from nextline.thread_safe_event import ThreadSafeAsyncioEvent
 
 ##__________________________________________________________________||
 @pytest.mark.asyncio
@@ -69,5 +72,63 @@ async def test_recive_the_most_recent_item(obj):
 
     # receive 'C', which was put before task_receive_2 started
     assert [pre_items[-1], *items] == await task_receive_2
+
+@pytest.mark.asyncio
+async def test_subscribe_after_end(obj):
+    # Note: This test might be unnecessary. It is more useful to test
+    # what happens if subscribed after closed.
+
+    items = list(range(10))
+    task_send = asyncio.create_task(async_send(obj, items))
+    await task_send
+
+    task_receive = asyncio.create_task(async_receive(obj))
+    await task_receive
+
+##__________________________________________________________________||
+@pytest.mark.asyncio
+async def test_thread(obj):
+    '''test if the issue is resovled
+    https://github.com/simonsobs/nextline/issues/2
+
+    not perfect. needs to be revised.
+    '''
+
+    def send(obj, event, items):
+        for i in items:
+            obj.put(i)
+            # time.sleep(0.00001)
+        obj.put(None)
+
+    async def set_event(obj, event, at=5):
+        async for i in obj.subscribe():
+            if i is None or i >= at:
+                event.set()
+            if i is None:
+                break
+        return
+
+    items = list(range(2000))
+    event = ThreadSafeAsyncioEvent()
+    send = partial(send, obj, event, items)
+    t = threading.Thread(target=send, daemon=True)
+    t.start()
+
+    task_set_event = asyncio.create_task(set_event(obj, event))
+
+    ntasks = 5
+    tasks = []
+
+    await event.wait()
+
+    for i in range(ntasks):
+        tasks.append(asyncio.create_task(async_receive(obj)))
+
+    for i in range(ntasks):
+        received = await tasks[i]
+        if received:
+            assert received == items[items.index(received[0]):]
+
+    t.join()
 
 ##__________________________________________________________________||
