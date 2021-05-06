730477727308f3b5d2d922a4b215b9238cc22f89
Tai Sakuma <tai.sakuma@gmail.com>
2021-05-06 10:46:13 -0400

    wait for prompt in test_simple.py

--- tests/scenarios/test_simple.py
+++ tests/scenarios/test_simple.py
@@ -21,14 +21,16 @@ async def test_run():
     nextline.run()
 
     async for global_state in nextline.subscribe_global_state():
-        print(global_state)
         if global_state == 'running':
             break
 
     async for thread_asynctask_ids in nextline.subscribe_thread_asynctask_ids():
-        print(thread_asynctask_ids)
         if thread_asynctask_ids:
             thread_asynctask_id = thread_asynctask_ids[0]
+            break
+
+    async for state in nextline.subscribe_thread_asynctask_state(thread_asynctask_id):
+        if state['prompting']:
             break
 
     nextline.send_pdb_command(thread_asynctask_id, 'continue')
