a0dbb48ee5bbb8369cd69f8656e5c5eac2381964
Tai Sakuma <tai.sakuma@gmail.com>
2021-05-07 17:18:31 -0400

    clean code in UniqThreadTaskIdComposer

--- nextline/trace.py
+++ nextline/trace.py
@@ -243,16 +243,14 @@ class UniqThreadTaskIdComposer:
     # task_ids: set()
 
     def __init__(self):
-        self.thread_ident_task_obj_id_dict = {}
-        # key: (threading.get_ident(), id(asyncio.current_task()))
-        # value: (thread_id, task_id)
 
-        self.thread_task_id_dict = {}
-        # key: (thread_id, task_id)
-        # value: (threading.get_ident(), id(asyncio.current_task()))
+        self.non_uniq_id_dict = {} # non_uniq_id -> uniq_id
+        self.uniq_id_dict = {} # uniq_id -> non_uniq_id
+        # uniq_id = (thread_id, task_id)
+        # non_uniq_id = (threading.get_ident(), id(asyncio.current_task()))
 
-        self.thread_ident_dict = {} # key: threading.get_ident(), value: thread_id
-        self.thread_id_dict = {} # key: thread_id, value: T
+        self.non_uniq_thread_id_dict = {} # threading.get_ident() -> thread_id
+        self.thread_id_dict = {} # thread_id -> ThreadSpecifics
 
         self.thread_id_counter = count().__next__
         self.thread_id_counter() # consume 0
@@ -269,68 +267,93 @@ class UniqThreadTaskIdComposer:
             not in an async task, the async task ID will be None.
         """
 
-        thread_ident, task_obj_id = self._compose_possibly_recycled_ids()
+        non_uniq_id = self._compose_non_uniq_id()
+
+        uniq_id = self._find_previsouly_composed_uniq_id(non_uniq_id)
+        if uniq_id:
+            return uniq_id
+
+        thread_id = self._compose_thread_id(non_uniq_id)
+        task_id = self._compose_task_id(non_uniq_id)
+        uniq_id = (thread_id, task_id)
+        return uniq_id
+
+    def exited(self, thread_task_id):
+        thread_id, task_id = thread_task_id
+        with self.condition:
+            non_uniq_thread_id, non_uniq_task_id = self.uniq_id_dict.pop(thread_task_id)
+            self.non_uniq_id_dict.pop((non_uniq_thread_id, non_uniq_task_id))
+            self.thread_id_dict[thread_id].task_ids.remove(task_id)
+            if not self.thread_id_dict[thread_id].task_ids:
+                self.non_uniq_thread_id_dict.pop(non_uniq_thread_id)
+
+    def _compose_non_uniq_id(self):
+
+        non_uniq_thread_id = threading.get_ident()
+        # the "thread identifier", which can be recycled after a thread exits
+        # https://docs.python.org/3/library/threading.html#threading.get_ident
+
+        non_uniq_task_id = None
+        try:
+            non_uniq_task_id = id(asyncio.current_task())
+            # the id of the task object, which can be also recycled
+            # https://docs.python.org/3/library/functions.html#id
+        except RuntimeError:
+            # no running event loop
+            pass
+
+        return non_uniq_thread_id, non_uniq_task_id
+
+    def _find_previsouly_composed_uniq_id(self, non_uniq_id):
+        try:
+            return self.non_uniq_id_dict[non_uniq_id]
+        except KeyError:
+            return None
+
+    def _compose_thread_id(self, non_uniq_id):
+
+        non_uniq_thread_id = non_uniq_id[0]
 
         try:
-            return self.thread_ident_task_obj_id_dict[(thread_ident, task_obj_id)]
+            return self.non_uniq_thread_id_dict[non_uniq_thread_id]
         except KeyError:
             pass
 
-        thread_id = self.thread_ident_dict.get(thread_ident, None)
-        if not thread_id:
-            thread_id = self.thread_id_counter()
+        thread_id = self.thread_id_counter()
 
-            task_id_counter = count().__next__
-            task_id_counter() # consume 0
+        task_id_counter = count().__next__
+        task_id_counter() # consume 0
 
-            thread_specifics = self.ThreadSpecifics(
-                    thread_ident=thread_ident,
-                    task_id_counter=task_id_counter,
-                    task_ids=set()
-                )
+        thread_specifics = self.ThreadSpecifics(
+                thread_ident=non_uniq_thread_id,
+                task_id_counter=task_id_counter,
+                task_ids=set()
+            )
 
-            with self.condition:
-                self.thread_ident_dict[thread_ident] = thread_id
-                self.thread_id_dict[thread_id] = thread_specifics
+        with self.condition:
+            self.non_uniq_thread_id_dict[non_uniq_thread_id] = thread_id
+            self.thread_id_dict[thread_id] = thread_specifics
+        return thread_id
+
+
+    def _compose_task_id(self, non_uniq_id):
+
+        non_uniq_thread_id, non_uniq_task_id = non_uniq_id
+
+        thread_id = self.non_uniq_thread_id_dict[non_uniq_thread_id]
 
         thread_specifics = self.thread_id_dict[thread_id]
 
         task_id =  None
-        if task_obj_id:
+        if non_uniq_task_id:
             task_id = thread_specifics.task_id_counter()
         thread_specifics.task_ids.add(task_id)
         thread_task_id = (thread_id, task_id)
 
         with self.condition:
-            self.thread_ident_task_obj_id_dict[(thread_ident, task_obj_id)] = thread_task_id
-            self.thread_task_id_dict[thread_task_id] = (thread_ident, task_obj_id)
+            self.non_uniq_id_dict[(non_uniq_thread_id, non_uniq_task_id)] = thread_task_id
+            self.uniq_id_dict[thread_task_id] = (non_uniq_thread_id, non_uniq_task_id)
 
-        return thread_task_id
-
-    def exited(self, thread_task_id):
-        thread_id, task_id = thread_task_id
-        with self.condition:
-            thread_ident, task_obj_id = self.thread_task_id_dict.pop(thread_task_id)
-            self.thread_ident_task_obj_id_dict.pop((thread_ident, task_obj_id))
-            self.thread_id_dict[thread_id].task_ids.remove(task_id)
-            if not self.thread_id_dict[thread_id].task_ids:
-                self.thread_ident_dict.pop(thread_ident)
-
-    def _compose_possibly_recycled_ids(self):
-
-        thread_ident = threading.get_ident()
-        # can be recycled after a thread exits
-        # https://docs.python.org/3/library/threading.html#threading.get_ident
-
-        task_obj_id = None
-        try:
-            task_obj_id = id(asyncio.current_task())
-        except RuntimeError:
-            # no running event loop
-            pass
-        # can be also recycled
-        # https://docs.python.org/3/library/functions.html#id
-
-        return thread_ident, task_obj_id
+        return task_id
 
 ##__________________________________________________________________||
