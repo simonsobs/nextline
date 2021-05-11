3815c8d6e90bbf550747c371da9d7348301fde03
Tai Sakuma <tai.sakuma@gmail.com>
2021-05-11 17:48:23 -0400

    clean code

--- nextline/main.py
+++ nextline/main.py
@@ -10,21 +10,52 @@ from .utils import QueueDist
 
 ##__________________________________________________________________||
 class Nextline:
+    """Nextline allows line-by-line execution of concurrent Python scripts
+
+    Nextline supports concurrency with threading and asyncio. It uses
+    an instance of Pdb for each thread and async task.
+
+    Note
+    ----
+    The running asyncio event loop must exists when Nextline is
+    instantiated.
+
+    Parameters
+    ----------
+    statement : str
+        A python code as a string
+
+    """
+
     def __init__(self, statement):
         self.statement = statement
         self.queue_global_state = QueueDist()
         self.registry = Registry()
-        self.state = Initialized(nextline=self)
+        self._state = Initialized(nextline=self)
 
     @property
-    def global_state(self):
-        return self.state.name
+    def global_state(self) -> str:
+        """state, e.g., "initialized", "running", "finished"
+        """
+        return self._state.name
 
     def run(self):
-        self.state = self.state.run(statement=self.statement)
+        """run the script
+        """
+        self._state = self._state.run(statement=self.statement)
 
-    def finished(self, t):
-        self.state = self.state.finished(t)
+    async def wait(self):
+        await self._state.wait()
+        await self.registry.close()
+        await self.queue_global_state.close()
+
+    def _change_state(self, state):
+        """change the state
+
+        This method is to be called by state objects. It is used to
+        change from Running to Finished.
+        """
+        self._state = state
 
     async def subscribe_global_state(self):
         async for y in self.queue_global_state.subscribe():
@@ -37,6 +68,9 @@ class Nextline:
     async def subscribe_thread_asynctask_state(self, thread_asynctask_id):
         async for y in self.registry.subscribe_thread_asynctask_state(thread_asynctask_id):
             yield y
+
+    def send_pdb_command(self, thread_asynctask_id, command):
+        self._state.send_pdb_command(thread_asynctask_id, command)
 
     def get_source(self, file_name=None):
         if not file_name or file_name == '<string>':
@@ -53,17 +87,6 @@ class Nextline:
             return lines[line_no - 1]
         return ''
 
-    def send_pdb_command(self, thread_asynctask_id, command):
-        self.state.send_pdb_command(thread_asynctask_id, command)
-
-    async def wait(self):
-        async for s in self.subscribe_global_state():
-            if s == 'finished':
-                break
-        await self.state.wait()
-        await self.registry.close()
-        await self.queue_global_state.close()
-
 ##__________________________________________________________________||
 class State:
     """The base class of the states
@@ -71,14 +94,29 @@ class State:
     def __init__(self, nextline):
         self.nextline = nextline
         self.nextline.queue_global_state.put(self.name)
+        self.thread = None
+
     def run(self, statement):
         return self
+
     async def wait(self):
         pass
-    def finished(self, t):
-        return self
+
     def send_pdb_command(self, thread_asynctask_id, command):
         pass
+
+    async def wait(self):
+        if self.thread:
+            await self._join(self.thread)
+
+    async def _join(self, thread):
+        try:
+            await asyncio.to_thread(thread.join)
+        except AttributeError:
+            # for Python 3.8
+            # to_thread() is new in Python 3.9
+            loop = asyncio.get_running_loop()
+            await loop.run_in_executor(None, thread.join)
 
 class Initialized(State):
     name = "initialized"
@@ -93,23 +131,25 @@ class Running(State):
     def __init__(self, nextline, statement):
         super().__init__(nextline)
 
-        self.loop = asyncio.get_running_loop()
-        self.statement = statement
-        self.registry = nextline.registry
-        self.trace = Trace(registry=self.registry)
-        self.pdb_ci_registry = self.trace.pdb_ci_registry
+        trace = Trace(registry=nextline.registry)
+        self.pdb_ci_registry = trace.pdb_ci_registry
 
-        if isinstance(self.statement, str):
-            cmd = compile(self.statement, '<string>', 'exec')
+        if isinstance(statement, str):
+            cmd = compile(statement, '<string>', 'exec')
         else:
-            cmd = self.statement
-        self.t = threading.Thread(target=self._execute_statement_with_trace, args=(cmd, ), daemon=True)
-        self.t.start()
+            cmd = statement
 
-    def _execute_statement_with_trace(self, cmd):
+        self.thread = threading.Thread(
+            target=self._exec,
+            args=(cmd, trace, self._done),
+            daemon=True
+        )
+        self.thread.start()
+
+    def _exec(self, cmd, trace, done):
         trace_org = sys.gettrace()
-        threading.settrace(self.trace)
-        sys.settrace(self.trace)
+        threading.settrace(trace)
+        sys.settrace(trace)
         try:
             exec(cmd)
         except BaseException as e:
@@ -118,11 +158,12 @@ class Running(State):
         finally:
             sys.settrace(trace_org)
             threading.settrace(trace_org)
+            done()
 
-        self.nextline.finished(self.t)
-
-    def finished(self, t):
-        return Finished(nextline=self.nextline, t=t)
+    def _done(self):
+        # to be called at the end of self._exec()
+        next_state = Finished(nextline=self.nextline, thread=self.thread)
+        self.nextline._change_state(next_state)
 
     def send_pdb_command(self, thread_asynctask_id, command):
         pdb_ci = self.pdb_ci_registry.get_ci(thread_asynctask_id)
@@ -131,16 +172,8 @@ class Running(State):
 
 class Finished(State):
     name = "finished"
-    def __init__(self, nextline, t):
+    def __init__(self, nextline, thread):
         super().__init__(nextline)
-        self.t = t
-    async def wait(self):
-        try:
-            await asyncio.to_thread(self.t.join)
-        except AttributeError:
-            # for Python 3.8
-            # to_thread() is new in Python 3.9
-            loop = asyncio.get_running_loop()
-            await loop.run_in_executor(None, self.t.join)
+        self.thread = thread
 
 ##__________________________________________________________________||
