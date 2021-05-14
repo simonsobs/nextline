498d4972de40f865d406ff8c648794d7b3cfbc09
Tai Sakuma <tai.sakuma@gmail.com>
2021-05-14 17:25:11 -0400

    update test_state.py

--- tests/test_state.py
+++ tests/test_state.py
@@ -3,7 +3,7 @@ import time
 import pytest
 from unittest.mock import Mock
 
-from nextline.registry import PdbCIRegistry
+from nextline.registry import Registry, PdbCIRegistry
 from nextline.state import (
     Initialized,
     Running,
@@ -33,39 +33,55 @@ def monkey_patch_trace(monkeypatch):
     yield mock_class
 
 @pytest.fixture()
+async def wrap_registry(monkeypatch):
+    mock_class = Mock(side_effect=lambda : Mock(wraps=Registry()))
+    monkeypatch.setattr('nextline.state.Registry', mock_class)
+    yield
+
+@pytest.fixture()
 def callback_exited():
     y = Mock()
     yield y
 
 ##__________________________________________________________________||
 @pytest.mark.asyncio
-async def test_state_normal_flow(callback_exited):
+async def test_state_transition(callback_exited):
 
     state = Initialized(SOURCE)
     assert isinstance(state, Initialized)
-    assert "initialized" == state.registry.state_name
 
     state = state.run(exited=callback_exited)
     assert isinstance(state, Running)
-    assert state.registry.state_name in ("running", "exited")
 
     state = await state.finish()
     assert isinstance(state, Finished)
-    assert "finished" == state.registry.state_name
 
     state = await state.close()
     assert isinstance(state, Closed)
-    assert "closed" == state.registry.state_name
+
+    # The existed state is received by the callback
+    assert 1 == callback_exited.call_count
+    state, *_ = callback_exited.call_args.args
+    assert isinstance(state, Exited)
 
 @pytest.mark.asyncio
-async def test_callback_exited(callback_exited):
-    state = Initialized(SOURCE)
-    state = state.run(exited=callback_exited)
-    state = await state.finish()
+async def test_register_state_name(callback_exited, wrap_registry):
 
-    assert 1 == callback_exited.call_count
-    state = callback_exited.call_args.args[0]
-    assert isinstance(state, Exited)
+    state = Initialized(SOURCE)
+    assert isinstance(state, Initialized)
+
+    state = state.run(exited=callback_exited)
+    assert isinstance(state, Running)
+
+    state = await state.finish()
+    assert isinstance(state, Finished)
+
+    state = await state.close()
+    assert isinstance(state, Closed)
+
+    expected = ['initialized', 'running', 'exited', 'finished', 'closed']
+    actual = [a.args[0] for a in state.registry.register_state_name.call_args_list]
+    assert expected == actual
 
 ##__________________________________________________________________||
 @pytest.mark.asyncio
