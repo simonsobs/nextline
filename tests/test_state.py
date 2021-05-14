437f554da51b9392033fa73d5b4d9a4f53e775c6
Tai Sakuma <tai.sakuma@gmail.com>
2021-05-13 20:05:29 -0400

    add test_state.py

---
+++ tests/test_state.py
@@ -0,0 +1,55 @@
+import time
+
+import pytest
+from unittest.mock import Mock
+
+from nextline.registry import PdbCIRegistry
+from nextline.state import (
+    Initialized,
+    Running,
+    Exited,
+    Finished
+)
+
+##__________________________________________________________________||
+SOURCE = """
+import time
+time.sleep(0.1)
+""".strip()
+
+##__________________________________________________________________||
+@pytest.fixture(autouse=True)
+def monkey_patch_trace(monkeypatch):
+    mock_instance = Mock()
+    mock_instance.return_value = None
+    mock_instance.pdb_ci_registry = Mock(spec=PdbCIRegistry)
+    mocak_class = Mock(return_value=mock_instance)
+    monkeypatch.setattr('nextline.state.Trace', mocak_class)
+    yield mocak_class
+
+@pytest.fixture()
+def callback_exited():
+    y = Mock()
+    yield y
+
+##__________________________________________________________________||
+@pytest.mark.asyncio
+async def test_normal_flow(callback_exited):
+    state = Initialized(SOURCE)
+    assert isinstance(state, Initialized)
+    state = state.run(exited=callback_exited)
+    assert isinstance(state, Running)
+    state = await state.wait()
+    assert isinstance(state, Finished)
+
+@pytest.mark.asyncio
+async def test_callback_exited(callback_exited):
+    state = Initialized(SOURCE)
+    state = state.run(exited=callback_exited)
+    state = await state.wait()
+
+    assert 1 == callback_exited.call_count
+    state = callback_exited.call_args.args[0]
+    assert isinstance(state, Exited)
+
+##__________________________________________________________________||
