a07ff9af63c1d266f79146c3d65cb42908954e30
Tai Sakuma <tai.sakuma@gmail.com>
2021-05-25 13:42:18 -0400

    add tests for CoroutineRunner

---
+++ tests/utils/test_coro_runner.py
@@ -0,0 +1,36 @@
+import asyncio
+
+import pytest
+from unittest.mock import AsyncMock
+
+from nextline.utils import CoroutineRunner
+
+##__________________________________________________________________||
+@pytest.fixture()
+async def obj():
+    y = CoroutineRunner()
+    yield y
+
+@pytest.mark.asyncio
+async def test_repr(obj):
+    repr(obj)
+
+@pytest.mark.asyncio
+async def test_async(obj):
+    afunc = AsyncMock()
+    coro = afunc()
+    task = obj.run(coro)
+    await task
+    assert 1 == afunc.await_count
+
+@pytest.mark.asyncio
+async def test_thread(obj):
+    def run():
+        afunc = AsyncMock()
+        coro = afunc()
+        ret = obj.run(coro)
+        assert ret is None
+        assert 1 == afunc.await_count
+    await asyncio.to_thread(run)
+
+##__________________________________________________________________||
