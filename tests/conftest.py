49dff22aa8c8385fedf556e89d08d19bac99f21b
Tai Sakuma <tai.sakuma@gmail.com>
2022-02-04 11:18:58 -0500

    clean code

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,8 @@ import pytest
 
 ##__________________________________________________________________||
 @pytest.fixture(autouse=True)
-def recover_trace(monkeypatch):
+def recover_trace():
+    """Set the original trace funciton back after each test"""
     trace_org = sys.gettrace()
     yield
     sys.settrace(trace_org)
