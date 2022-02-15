0aaab194cd33e7728a4c181245bae23c96dd2ee7
Tai Sakuma <tai.sakuma@gmail.com>
2022-02-14 20:03:35 -0500

    clean code

--- nextline/utils/trace.py
+++ nextline/utils/trace.py
@@ -12,12 +12,8 @@ class TraceSingleThreadTask:
     """Dispatch a new trace function for each thread or asyncio task"""
 
     def __init__(self, factory: Callable[[], TraceFunc]):
-
-        self._wrapped_factory = factory
-
-        self._trace_map: Dict[
-            Union[Thread, Task], TraceFunc
-        ] = WeakKeyDictionary()
+        self._factory = factory
+        self._map: Dict[Union[Thread, Task], TraceFunc] = WeakKeyDictionary()
 
     def __call__(
         self, frame: FrameType, event: str, arg: Any
@@ -25,14 +21,14 @@ class TraceSingleThreadTask:
 
         key = self._current_task_or_thread()
 
-        trace = self._trace_map.get(key)
+        trace = self._map.get(key)
         if not trace:
-            trace = self._wrapped_factory()
-            self._trace_map[key] = trace
+            trace = self._factory()
+            self._map[key] = trace
 
         return trace(frame, event, arg)
 
-    def _current_task_or_thread(self) -> Union[Thread, Task]:
+    def _current_task_or_thread(self) -> Union[Task, Thread]:
         try:
             task = current_task()
         except RuntimeError:
