28051490456369df5272252bc71a10947a2bb214
Tai Sakuma <tai.sakuma@gmail.com>
2022-02-21 14:23:44 -0500

    update test

--- tests/trace/test_skip_module.py
+++ tests/trace/test_skip_module.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import Mock
 
-from typing import Set
+from typing import Union, Set
 
 from nextline.call import call_with_trace
 from nextline.trace import TraceSkipModule
@@ -9,29 +9,25 @@ from nextline.types import TraceFunc
 
 from .funcs import summarize_trace_calls, TraceSummaryType
 
-from . import module_a, module_b
+from . import module_a
 
 
 def test_one(
-    obj_call_summary: TraceSummaryType,
-    trace_call_summary: TraceSummaryType,
+    target: TraceSummaryType,
+    probe: TraceSummaryType,
+    ref: TraceSummaryType,
+    modules_to_skip: Set[str],
 ):
-    expected = {
-        func.__name__,
-        # module_a.func_a.__name__,
-        module_b.func_b.__name__,
-    }
-    assert expected <= obj_call_summary.call.func
-    assert expected <= trace_call_summary.return_.func
-
-    assert not obj_call_summary.return_.func
+    assert ref.call.module
+    assert ref.return_.module
+    assert ref.call.module == target.call.module
+    assert not target.return_.module
+    assert ref.call.module - modules_to_skip == probe.call.module
+    assert ref.call.module - modules_to_skip == probe.return_.module
 
 
-@pytest.fixture()
-def mock_trace():
-    y = Mock()
-    y.return_value = y
-    yield y
+def func():
+    module_a.func_a()
 
 
 @pytest.fixture(params=[set(), {module_a.__name__}])
@@ -41,9 +37,42 @@ def modules_to_skip(request):
 
 
 @pytest.fixture()
-def obj(mock_trace: Mock, modules_to_skip: Set[str]):
+def target(target_trace_func: Union[TraceFunc, Mock], run_target):
+    _ = run_target
+    y = summarize_trace_calls(target_trace_func)
+    yield y
+
+
+@pytest.fixture()
+def probe(probe_trace_func: Mock, run_target):
+    _ = run_target
+    y = summarize_trace_calls(probe_trace_func)
+    yield y
+
+
+@pytest.fixture()
+def ref(ref_trace_func: Mock, run_ref):
+    _ = run_ref
+    y = summarize_trace_calls(ref_trace_func)
+    yield y
+
+
+@pytest.fixture()
+def run_target(target_trace_func: Union[TraceFunc, Mock]):
+    call_with_trace(func=func, trace=target_trace_func, thread=False)
+    yield
+
+
+@pytest.fixture()
+def run_ref(ref_trace_func: Union[TraceFunc, Mock]):
+    call_with_trace(func=func, trace=ref_trace_func, thread=False)
+    yield
+
+
+@pytest.fixture()
+def target_trace_func(probe_trace_func: Mock, modules_to_skip: Set[str]):
     y = TraceSkipModule(
-        trace=mock_trace,
+        trace=probe_trace_func,
         skip=modules_to_skip,
     )
     y = Mock(wraps=y)
@@ -51,18 +80,14 @@ def obj(mock_trace: Mock, modules_to_skip: Set[str]):
 
 
 @pytest.fixture()
-def obj_call_summary(obj: TraceFunc):
-    call_with_trace(func=func, trace=obj, thread=False)
-    y = summarize_trace_calls(obj)
+def probe_trace_func():
+    y = Mock()
+    y.return_value = y
     yield y
 
 
 @pytest.fixture()
-def trace_call_summary(obj_call_summary, mock_trace: Mock):
-    assert obj_call_summary is not None
-    y = summarize_trace_calls(mock_trace)
+def ref_trace_func():
+    y = Mock()
+    y.return_value = y
     yield y
-
-
-def func():
-    module_a.func_a()
