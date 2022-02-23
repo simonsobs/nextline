8cd1e7eb92957af8c8f8d4505adfea12b4779446
Tai Sakuma <tai.sakuma@gmail.com>
2022-02-23 09:03:15 -0500

    correct a typo

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 ##__________________________________________________________________||
 @pytest.fixture(autouse=True)
 def recover_trace():
-    """Set the original trace funciton back after each test"""
+    """Set the original trace function back after each test"""
     trace_org = sys.gettrace()
     yield
     sys.settrace(trace_org)
