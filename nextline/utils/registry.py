0bdba5b303b1f6608d56bb46a0800391b514b51a
Tai Sakuma <tai.sakuma@gmail.com>
2022-02-23 19:55:51 -0500

    remove commented out lines

--- nextline/utils/registry.py
+++ nextline/utils/registry.py
@@ -22,8 +22,6 @@ class Registry:
         self._loop = asyncio.get_running_loop()
         self._runner = CoroutineRunner()
         self._lock = threading.Condition()
-        # self._data = {}
-        # self._queue = {}
         self._aws = []
         self._map: Dict[str, DQ] = {}
 
@@ -34,10 +32,6 @@ class Registry:
         while self._map:
             _, dq = self._map.popitem()
             await dq.queue.close()
-        # while self._queue:
-        #     _, q = self._queue.popitem()
-        # await q.close()
-        # self._data.clear()
 
     def open_register(self, key: Hashable):
         """Create a register for an item"""
@@ -48,9 +42,6 @@ class Registry:
         self._open_register(key, [])
 
     def _open_register(self, key, init_data):
-        # if key in self._data:
-        #     raise Exception(f"register key already exists {key!r}")
-        # self._data[key] = init_data
         if key in self._map:
             self._map[key].data = init_data
         try:
@@ -63,54 +54,33 @@ class Registry:
             self._create_queue_from_another_thread(key, init_data)
 
     def close_register(self, key: Hashable):
-        # try:
-        #     del self._data[key]
-        # except KeyError:
-        #     warnings.warn(f"key not found: {key}")
         self._close_queue_from_another_thread(key)
 
     def register(self, key, item):
         """Replace the item in the register"""
-        # if key not in self._data:
-        #     raise Exception(f"register key does not exist {key!r}")
-
-        # self._data[key] = item
         self._map[key].data = item
-
         self._distribute(key, item)
 
     def register_list_item(self, key, item):
         """Add an item to the register"""
-        # if key not in self._data:
-        #     raise Exception(f"register key does not exist {key!r}")
-
         with self._lock:
-            # self._data[key].append(item)
-            # copy = self._data[key].copy()
             self._map[key].data.append(item)
             copy = self._map[key].data.copy()
-
         self._distribute(key, copy)
 
     def deregister_list_item(self, key, item):
         """Remove the item from the register"""
-        # if key not in self._data:
-        #     raise Exception(f"register key does not exist {key!r}")
-
         with self._lock:
             try:
-                # self._data[key].remove(item)
                 self._map[key].data.remove(item)
             except ValueError:
                 warnings.warn(f"item not found: {item}")
-            # copy = self._data[key].copy()
             copy = self._map[key].data.copy()
 
         self._distribute(key, copy)
 
     def get(self, key, default=None):
         """The item for the key. The default if the key doesn't exist"""
-        # return self._data.get(key, default)
         if dp := self._map.get(key):
             return dp.data
         else:
@@ -121,12 +91,6 @@ class Registry:
 
         This method needs to be called in the initial thread.
         """
-        # queue = self._queue.get(key)
-        # if not queue:
-        #     self._create_queue(key)
-        #     queue = self._queue.get(key)
-        # async for y in queue.subscribe():
-        #     yield y
         dq = self._map.get(key)
         if not dq:
             self._create_queue(key)
@@ -150,12 +114,9 @@ class Registry:
 
         To be run in the initial thread
         """
-        # if key in self._queue:
-        #     return
         if key in self._map:
             return
         q = QueueDist()
-        # self._queue[key] = q
         self._map[key] = DQ(data=init_data, queue=q)
 
     def _close_queue_from_another_thread(self, key):
@@ -171,9 +132,6 @@ class Registry:
 
         To be run in the initial thread
         """
-        # queue = self._queue.pop(key, None)
-        # if queue:
-        #     await queue.close()
         dq = self._map.pop(key, None)
         if dq:
             await dq.queue.close()
@@ -182,7 +140,6 @@ class Registry:
         """Send item to subscribers"""
 
         async def put():
-            # self._queue[key].put(item)
             self._map[key].queue.put(item)
 
         self._run_in_the_initial_thread(put())
