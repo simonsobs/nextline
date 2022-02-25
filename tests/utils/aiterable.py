41fcd4360543d80cdb3f68a40946089d45831162
Tai Sakuma <tai.sakuma@gmail.com>
2022-02-25 10:08:16 -0500

    add type hint

--- tests/utils/aiterable.py
+++ tests/utils/aiterable.py
@@ -1,7 +1,8 @@
 import asyncio
+from typing import AsyncIterable, Iterable
 
 
-async def aiterable(iterable):
+async def aiterable(iterable: Iterable) -> AsyncIterable:
     '''Wrap iterable so can be used with "async for"'''
     for i in iterable:
         await asyncio.sleep(0)  # let other tasks run
