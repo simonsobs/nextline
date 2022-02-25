4f5925edf5c7b6af1e2445acfb0d9149cddcf861
Tai Sakuma <tai.sakuma@gmail.com>
2022-02-25 17:59:25 -0500

    remove register_list_item()

--- nextline/utils/registry.py
+++ nextline/utils/registry.py
@@ -1,6 +1,4 @@
-import threading
 from collections import defaultdict
-import warnings
 from typing import DefaultDict
 
 from .queuedist import QueueDist
@@ -10,8 +8,6 @@ class Registry:
     """Subscribable asynchronous thread-safe registers"""
 
     def __init__(self):
-        self._lock = threading.Condition()
-
         self._map: DefaultDict[str, QueueDist] = defaultdict(QueueDist)
 
     def close(self):
@@ -22,10 +18,6 @@ class Registry:
 
     def open_register(self, key):
         """Create a register for an item"""
-        _ = self._map[key]
-
-    def open_register_list(self, key):
-        """Create a register for an item list"""
         _ = self._map[key]
 
     def close_register(self, key):
@@ -41,25 +33,6 @@ class Registry:
     def register(self, key, item):
         """Replace the item in the register"""
         self._map[key].put(item)
-
-    def register_list_item(self, key, item):
-        """Add an item to the register"""
-        with self._lock:
-            dp = self._map[key]
-            copy = (dp.get() or ()) + (item,)
-            dp.put(copy)
-
-    def deregister_list_item(self, key, item):
-        """Remove the item from the register"""
-        with self._lock:
-            dp = self._map[key]
-            copy = list(dp.get())
-            try:
-                copy.remove(item)
-            except ValueError:
-                warnings.warn(f"item not found: {item}")
-            copy = tuple(copy)
-            dp.put(copy)
 
     def get(self, key, default=None):
         """The item for the key. The default if the key doesn't exist"""
