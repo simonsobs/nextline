94e9a27ada6f1e5dba2ee6ddd520fe0dd90c144a
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-03 14:21:24 -0500

    remove CoroutineRunner

--- nextline/utils/__init__.py
+++ nextline/utils/__init__.py
@@ -1,7 +1,6 @@
 from .queuedist import QueueDist  # noqa: F401
 from .thread_task_id import UniqThreadTaskIdComposer  # noqa: F401
 from .thread_safe_event import ThreadSafeAsyncioEvent  # noqa: F401
-from .coro_runner import CoroutineRunner  # noqa: F401
 from .loop import ToLoop  # noqa: F401
 from .registry import Registry  # noqa: F401
 from .thread_exception import ExcThread  # noqa: F401
