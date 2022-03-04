fc5ba047168eb7140780516854a4019563ca1fb9
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-04 09:22:38 -0500

    remove an unused attribute

--- nextline/registry.py
+++ nextline/registry.py
@@ -5,19 +5,16 @@ class PdbCIRegistry:
     """Hold the list of active pdb command interfaces"""
 
     def __init__(self):
-        self.pdb_cis = []
         self._dict = {}
         self.condition = threading.Condition()
 
     def add(self, trace_id, pdb_ci):
         with self.condition:
             self._dict[trace_id] = pdb_ci
-            self.pdb_cis.append(pdb_ci)
 
     def remove(self, trace_id):
         with self.condition:
             pdb_ci = self._dict.pop(trace_id)
-            self.pdb_cis.remove(pdb_ci)
 
     def get_ci(self, trace_id):
         with self.condition:
