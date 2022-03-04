5c0d0d5623834762354f26afe74422fc65a9f8c9
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-04 09:08:39 -0500

    rename, remove rulers

--- nextline/registry.py
+++ nextline/registry.py
@@ -1,7 +1,6 @@
 import threading
 
 
-##__________________________________________________________________||
 class PdbCIRegistry:
     """Hold the list of active pdb command interfaces"""
 
@@ -10,19 +9,16 @@ class PdbCIRegistry:
         self._dict = {}
         self.condition = threading.Condition()
 
-    def add(self, thread_task_id, pdb_ci):
+    def add(self, trace_id, pdb_ci):
         with self.condition:
-            self._dict[thread_task_id] = pdb_ci
+            self._dict[trace_id] = pdb_ci
             self.pdb_cis.append(pdb_ci)
 
-    def remove(self, thread_task_id):
+    def remove(self, trace_id):
         with self.condition:
-            pdb_ci = self._dict.pop(thread_task_id)
+            pdb_ci = self._dict.pop(trace_id)
             self.pdb_cis.remove(pdb_ci)
 
-    def get_ci(self, thread_task_id):
+    def get_ci(self, trace_id):
         with self.condition:
-            return self._dict.get(thread_task_id, None)
-
-
-##__________________________________________________________________||
+            return self._dict.get(trace_id, None)
