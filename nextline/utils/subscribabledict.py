9ed8d163de5b40638f2bf1aa2a22893439b7b012
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-09 10:39:43 -0500

    add docstrings, reorder methods

--- nextline/utils/subscribabledict.py
+++ nextline/utils/subscribabledict.py
@@ -9,44 +9,57 @@ _VT = TypeVar("_VT")
 
 
 class SubscribableDict(MutableMapping):
-    """Dict with async generator that yields changes"""
+    """Dict with async generator that yields values as they change"""
 
     def __init__(self):
         self._map: DefaultDict[_KT, QueueDist] = defaultdict(QueueDist)
 
+    def subscribe(self, key: _KT) -> AsyncGenerator[_VT, None]:
+        """Async generator that yields values for the key as they are set
+
+        Yields immediately the current value for the key, wait for new values
+        and yield them as they are set. Wait for the first value for the key,
+        if the key doesn't exist; KeyError won't be raised.
+        """
+        q = self._map[key]
+        return q.subscribe()
+
     def __getitem__(self, key: _KT) -> _VT:
+        """The current value for the key. KeyError if the key doesn't exist."""
         if q := self._map.get(key):
             return q.get()
         else:
             raise KeyError
 
+    def get(self, key: _KT, default: Optional[_VT] = None) -> Optional[_VT]:
+        """The current value for the key if the key exist, else the default"""
+        if q := self._map.get(key):
+            return q.get()
+        else:
+            return default
+
     def __setitem__(self, key: _KT, value: _VT) -> None:
+        """Set the value for the key, yielding the value in the generators"""
         self._map[key].put(value)
 
     def __delitem__(self, key: _KT) -> None:
+        """Remove the key, ending all subscriptions for the key
+
+        The async generators returned by the method `subscribe()` for the key
+        will return.
+        """
         q = self._map.pop(key, None)
         if q is None:
             return
         q.close()
+
+    def close(self):
+        """Remove all keys, ending all subscriptions for all keys"""
+        for key in list(self):
+            del self[key]
 
     def __iter__(self) -> Iterator[_KT]:
         return iter(self._map)
 
     def __len__(self) -> int:
         return len(self._map)
-
-    def close(self):
-        """End gracefully"""
-        for key in list(self):
-            del self[key]
-
-    def get(self, key: _KT, default: Optional[_VT] = None) -> Optional[_VT]:
-        """The item for the key. The default if the key doesn't exist"""
-        if q := self._map.get(key):
-            return q.get()
-        else:
-            return default
-
-    def subscribe(self, key: _KT) -> AsyncGenerator[_VT, None]:
-        q = self._map[key]
-        return q.subscribe()
