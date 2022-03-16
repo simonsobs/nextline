cd04be52dab67e53d7558253d2cbe7885a00c5fe
Tai Sakuma <tai.sakuma@gmail.com>
2022-03-16 11:39:40 -0400

    copy subscribe_stdout() from nextline-graphql

--- nextline/main.py
+++ nextline/main.py
@@ -1,10 +1,23 @@
 from __future__ import annotations
 
+import sys
+import io
+import datetime
 import linecache
 
-from typing import TYPE_CHECKING, Any, AsyncGenerator, Optional, Tuple
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    AsyncGenerator,
+    Optional,
+    Tuple,
+    Callable,
+    Union,
+    TextIO,
+)
 
 from .state import Machine
+from .utils import QueueDist
 
 if TYPE_CHECKING:
     from .pdb.proxy import PdbCIState
@@ -127,3 +140,63 @@ class Nextline:
 
     def subscribe(self, key) -> AsyncGenerator[Any, None]:
         return self.registry.subscribe(key)
+
+    def subscribe_stdout(self):
+        return subscribe_stdout()
+
+
+AGenDatetimeStr = AsyncGenerator[Tuple[datetime.datetime, str], None]
+
+
+class IOSubscription(io.TextIOWrapper):
+    def __init__(self, src: TextIO):
+        """Make output stream subscribable
+
+        The src needs to be replaced with the instance of this class. For
+        example, if the src is stdout,
+            sys.stdout = IOSubscription(sys.stdout)
+
+        NOTE: The code on the logic about the buffer copied from
+        https://github.com/alphatwirl/atpbar/blob/894a7e0b4d81aa7b/atpbar/stream.py#L54
+        """
+        self._queue = QueueDist()
+        self._src = src
+        self._buffer = ""
+
+    def write(self, s: str) -> int:
+
+        ret = self._src.write(s)
+        # TypeError if s isn't str as long as self._src is sys.stdout or
+        # sys.stderr.
+
+        self._buffer += s
+        if s.endswith("\n"):
+            self.flush()
+        return ret
+
+    def flush(self):
+        if not self._buffer:
+            return
+        now = datetime.datetime.now()
+        self._queue.put((now, self._buffer))
+        self._buffer = ""
+
+    async def subscribe(self) -> AGenDatetimeStr:
+        async for y in self._queue.subscribe():
+            yield y
+
+
+def create_subscribe_stdout() -> Callable[[], AGenDatetimeStr]:
+    stream: Union[IOSubscription, None] = None
+
+    async def subscribe_stdout() -> AGenDatetimeStr:
+        nonlocal stream
+        if not stream:
+            stream = sys.stdout = IOSubscription(sys.stdout)
+        async for y in stream.subscribe():
+            yield y
+
+    return subscribe_stdout
+
+
+subscribe_stdout = create_subscribe_stdout()
