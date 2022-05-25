a49949c264b436bd8da5696daa8f7474f5f78afc
Tai Sakuma <tai.sakuma@gmail.com>
2022-05-24 20:16:03 -0400

    update tests

--- tests/utils/test_subscribabledict.py
+++ tests/utils/test_subscribabledict.py
@@ -1,11 +1,10 @@
 import asyncio
-from string import ascii_lowercase
+from string import ascii_lowercase, ascii_uppercase
+import time
 
 import pytest
 
 from nextline.utils import SubscribableDict, to_thread
-
-from .aiterable import aiterable
 
 
 @pytest.fixture
@@ -128,76 +127,51 @@ async def test_last(obj: SubscribableDict[str, str], last: bool):
 
 
 @pytest.mark.asyncio
-async def test_one_():
-    obj: SubscribableDict[str, str] = SubscribableDict()
+async def test_init():
+    n_keys = 2
+    keys = ascii_uppercase[:n_keys]
+    data = {k: f"{k}-a" for k in keys}
+    obj: SubscribableDict[str, str] = SubscribableDict(data)
+    expected = [(data[k],) for k in keys]
 
-    key = "key_one"
-    items = ["item_one", "item_two", "item_three"]
-
-    async def subscribe(last=True):
-        return [y async for y in obj.subscribe(key, last=last)]
-
-    async def send():
-        async for i in aiterable(items):
-            obj[key] = i
-            assert i == obj[key]
-        assert 1 == len(obj)
-        assert [key] == list(obj)
-        del obj[key]
-        assert 0 == len(obj)
-        assert [] == list(obj)
-
-    results = await asyncio.gather(subscribe(), subscribe(False), send())
-    # NOTE: the effect of the option `last` is not tested
-    assert items == results[0]
-    assert items == results[1]
-
-    obj.close()
-
-
-@pytest.mark.parametrize("nitems", [0, 1, 2, 50])
-@pytest.mark.parametrize("nsubscribers", [0, 1, 2, 70])
-@pytest.mark.parametrize("thread", [True, False])
-@pytest.mark.parametrize("close_register", [True, False])
-@pytest.mark.asyncio
-async def test_matrix(close_register, thread, nsubscribers, nitems):
-    async def subscribe():
+    async def subscribe(key):
         return tuple([y async for y in obj.subscribe(key)])
 
-    def register():
-        for item in items:
+    async def put():
+        await asyncio.sleep(0.001)
+        for key in keys:
+            del obj[key]
+
+    *results, _ = await asyncio.gather(*(subscribe(k) for k in keys), put())
+    assert results == expected
+
+
+@pytest.mark.parametrize("n_keys", [0, 1, 3])
+@pytest.mark.parametrize("n_items", [0, 1, 2, 30])
+@pytest.mark.parametrize("n_subscribers", [0, 1, 2, 50])
+@pytest.mark.asyncio
+async def test_matrix(
+    obj: SubscribableDict[str, str],
+    n_keys: int,
+    n_items: int,
+    n_subscribers: int,
+):
+    keys = ascii_uppercase[:n_keys]
+    items = {k: tuple(f"{k}-{i+1}" for i in range(n_items)) for k in keys}
+
+    async def subscribe(key):
+        return tuple([y async for y in obj.subscribe(key)])
+
+    def put(key):
+        time.sleep(0.01)
+        for item in items[key]:
             obj[key] = item
-        if close_register:
-            if key in obj:
-                del obj[key]
+        obj.end(key)
 
-    async def aregister():
-        return register()
-
-    obj = SubscribableDict()
-
-    key = "item"
-    items = tuple([f"{key}-{i+1}" for i in range(nitems)])
-
-    # subscribe
-    tasks_subscribe = []
-    for i in range(nsubscribers):
-        task = asyncio.create_task(subscribe())
-        tasks_subscribe.append(task)
-
-    # register
-    if thread:
-        coro = to_thread(register)
-    else:
-        coro = aregister()
-    task_register = asyncio.create_task(coro)
-
-    await asyncio.gather(task_register)
-
-    task_close = to_thread(obj.close)
-
-    results = await asyncio.gather(*tasks_subscribe, task_close)
-    expected = items
-    actuals = results[:nsubscribers]
-    for actual in actuals:
-        assert actual == expected
+    results = await asyncio.gather(
+        *(subscribe(k) for k in keys for _ in range(n_subscribers)),
+        *(to_thread(put, k) for k in keys),
+    )
+    actual = results[: n_subscribers * n_keys]
+    expected = [items[k] for k in keys for _ in range(n_subscribers)]
+    assert actual == expected
