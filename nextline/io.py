2e86cc8cc60b15df629c32582900255782bc24c1
Tai Sakuma <tai.sakuma@gmail.com>
2022-05-25 16:59:31 -0400

    update the implementation of IOSubscription with a queue

--- nextline/io.py
+++ nextline/io.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 import io
+from queue import Queue
+from threading import Thread
 import datetime
 from collections import defaultdict
 
-from typing import TYPE_CHECKING, DefaultDict, Mapping, TextIO
+from typing import TYPE_CHECKING, Any, DefaultDict, Mapping, TextIO
 
 
 from .utils import SubscribableQueue, current_task_or_thread
@@ -12,7 +14,6 @@ from .types import StdoutInfo
 
 if TYPE_CHECKING:
     from asyncio import Task
-    from threading import Thread
 
 
 def create_key_factory(to_put):
@@ -41,13 +42,15 @@ class IOSubscription(io.TextIOWrapper):
         self._run_no_map = registry["run_no_map"]  # type: ignore
         self._trace_no_map = registry["trace_no_map"]  # type: ignore
 
-        id_composer = registry["trace_id_factory"]  # type: ignore
+        self._q: Queue[Any] = Queue()
+        self._thread = Thread(target=self._listen, daemon=True)
+        self._thread.start()
 
+        id_composer = registry["trace_id_factory"]  # type: ignore
         self._key_factory = create_key_factory(to_put=id_composer.has_id)
         self._s = Scratch(
             src=src,
-            put=self._put,
-            queue=self._queue,
+            queue=self._q,
             key_factory=self._key_factory,
         )
 
@@ -69,19 +72,27 @@ class IOSubscription(io.TextIOWrapper):
         self._queue.put(info)
 
     def close(self):
+        self._q.put(None)
+        self._q.join()
+        self._thread.join()
         self._queue.close()
 
     def subscribe(self):
         return self._queue.subscribe(last=False)
 
+    def _listen(self) -> None:
+        while m := self._q.get():
+            self._put(*m)
+            self._q.task_done()
+        self._q.task_done()
+
 
 class Scratch(io.TextIOWrapper):
-    def __init__(self, src: TextIO, queue, put, key_factory):
+    def __init__(self, src: TextIO, queue, key_factory):
         self._queue = queue
-        self._put = put
         self._src = src
         self._key_factory = key_factory
-        self._buffer: DefaultDict[Task | Thread, str] = defaultdict(str)
+        self._buffer: DefaultDict[Any, str] = defaultdict(str)
 
     def write(self, s: str) -> int:
 
@@ -94,9 +105,7 @@ class Scratch(io.TextIOWrapper):
 
         self._buffer[key] += s
         if s.endswith("\n"):
-            self._put(
-                key=key,
-                text=self._buffer.pop(key),
-                timestamp=datetime.datetime.now(),
+            self._queue.put(
+                (key, self._buffer.pop(key), datetime.datetime.now())
             )
         return ret
