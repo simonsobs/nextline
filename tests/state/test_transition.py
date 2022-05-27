afc3844874b46e21443c60612a6d64c984b2f67d
Tai Sakuma <tai.sakuma@gmail.com>
2022-05-27 11:35:28 -0400

    update a test

--- tests/state/test_transition.py
+++ tests/state/test_transition.py
@@ -1,20 +1,11 @@
-import sys
 import itertools
 
 import pytest
+from unittest.mock import Mock
 
-from nextline.state import (
-    Initialized,
-    Running,
-    Finished,
-    Closed,
-)
+from nextline.state import Initialized, Running, Finished, Closed
 from nextline.utils import SubscribableDict, ThreadTaskIdComposer
-
-SOURCE = """
-import time
-time.sleep(0.001)
-""".strip()
+from nextline.run import QCommands, QDone
 
 
 @pytest.fixture()
@@ -22,14 +13,12 @@ def registry():
     y = SubscribableDict()
     y["run_no_count"] = itertools.count().__next__
     y["trace_id_factory"] = ThreadTaskIdComposer()
-    y["create_capture_stdout"] = lambda _: sys.stdout
     yield y
     y.close()
 
 
 @pytest.mark.asyncio
 async def test_transition(registry):
-    registry["statement"] = SOURCE
 
     state = Initialized(registry=registry)
     assert isinstance(state, Initialized)
@@ -42,3 +31,18 @@ async def test_transition(registry):
 
     state = state.close()
     assert isinstance(state, Closed)
+
+
+@pytest.fixture(autouse=True)
+def monkey_patch_run(monkeypatch):
+    def mock_run(
+        registry: SubscribableDict,
+        q_commands: QCommands,
+        q_done: QDone,
+    ) -> None:
+        del registry, q_commands
+        q_done.put((None, None))
+
+    wrap = Mock(wraps=mock_run)
+    monkeypatch.setattr("nextline.state.run", wrap)
+    yield wrap
