a516038f947db02900ad679f6eab7dfd67241109
Tai Sakuma <tai.sakuma@gmail.com>
2022-06-04 12:34:20 -0400

    add _run_no_map and _trace_no_map

--- nextline/registrar.py
+++ nextline/registrar.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from asyncio import Task  # noqa F401
+from threading import Thread  # noqa F401
 import dataclasses
 import datetime
 import traceback
@@ -7,6 +9,8 @@ import json
 from weakref import WeakKeyDictionary
 
 from typing import TYPE_CHECKING
+from typing import MutableMapping  # noqa F401
+from typing_extensions import TypeAlias
 
 from .utils import SubscribableDict
 from .types import RunInfo
@@ -16,12 +20,17 @@ if TYPE_CHECKING:
 
 SCRIPT_FILE_NAME = "<string>"
 
+RunNoMap: TypeAlias = "MutableMapping[Task | Thread, int]"
+TraceNoMap: TypeAlias = "MutableMapping[Task | Thread, int]"
+
 
 class Registrar:
     def __init__(self, registry: SubscribableDict):
         self._registry = registry
-        self._registry["run_no_map"] = WeakKeyDictionary()
-        self._registry["trace_no_map"] = WeakKeyDictionary()
+        self._run_no_map: RunNoMap = WeakKeyDictionary()
+        self._trace_no_map: TraceNoMap = WeakKeyDictionary()
+        self._registry["run_no_map"] = self._run_no_map
+        self._registry["trace_no_map"] = self._trace_no_map
 
     def script_change(self, script: str, filename: str) -> None:
         self._registry["statement"] = script
