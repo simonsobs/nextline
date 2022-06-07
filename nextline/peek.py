01763f166c12a449a8e97118f3a044bc51c06961
Tai Sakuma <tai.sakuma@gmail.com>
2022-06-07 14:45:13 -0400

    replace CallableContextManager with Callable

--- nextline/peek.py
+++ nextline/peek.py
@@ -1,30 +1,25 @@
 from __future__ import annotations
 import sys
 from contextlib import contextmanager
-from typing import Any, ContextManager, TextIO
-
-
-class CallableContextManager(ContextManager):
-    def __call__(self, __s: str) -> Any:
-        ...
+from typing import Any, Callable, TextIO
 
 
 @contextmanager
-def peek_stdout_write(callback: CallableContextManager):
+def peek_stdout_write(callback: Callable[[str], Any]):
     textio = sys.stdout
     with peek_textio_write(textio, callback) as t:
         yield t
 
 
 @contextmanager
-def peek_stderr_write(callback):
+def peek_stderr_write(callback: Callable[[str], Any]):
     textio = sys.stderr
     with peek_textio_write(textio, callback) as t:
         yield t
 
 
 @contextmanager
-def peek_textio_write(textio: TextIO, callback: CallableContextManager):
+def peek_textio_write(textio: TextIO, callback: Callable[[str], Any]):
     org_write = textio.write
 
     def write(s: str, /) -> int:
@@ -34,7 +29,6 @@ def peek_textio_write(textio: TextIO, callback: CallableContextManager):
     textio.write = write  # type: ignore
 
     try:
-        with callback:
-            yield write
+        yield write
     finally:
         textio.write = org_write  # type: ignore
--- tests/test_peek.py
+++ tests/test_peek.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 import sys
 
 import pytest
-from unittest.mock import MagicMock, Mock, call
+from unittest.mock import Mock, call
 
 from nextline.peek import peek_stdout_write, peek_stderr_write
 
 
 def test_stdout(capsys):
-    callback = MagicMock()
+    callback = Mock()
 
     with peek_stdout_write(callback):
         print("foo")
@@ -19,15 +19,13 @@ def test_stdout(capsys):
     #     print(callback.call_args_list)
 
     assert [call("foo"), call("\n")] == callback.call_args_list
-    assert [call()] == callback.__enter__.call_args_list
-    assert [call(None, None, None)] == callback.__exit__.call_args_list
 
     captured = capsys.readouterr()
     assert "foo\nbar\n" == captured.out
 
 
 def test_stderr(capsys):
-    callback = MagicMock()
+    callback = Mock()
 
     with peek_stderr_write(callback):
         print("foo", file=sys.stderr)
@@ -38,83 +36,49 @@ def test_stderr(capsys):
     #     print(callback.call_args_list)
 
     assert [call("foo"), call("\n")] == callback.call_args_list
-    assert [call()] == callback.__enter__.call_args_list
-    assert [call(None, None, None)] == callback.__exit__.call_args_list
 
     captured = capsys.readouterr()
     assert "foo\nbar\n" == captured.err
 
 
 def test_stdout_target(capsys):
-    callback = MagicMock()
+    callback = Mock()
 
     with peek_stdout_write(callback) as t:
         t("foo")
 
     assert [call("foo")] == callback.call_args_list
-    assert [call()] == callback.__enter__.call_args_list
-    assert [call(None, None, None)] == callback.__exit__.call_args_list
 
     captured = capsys.readouterr()
     assert "foo" == captured.out
 
 
 def test_stderr_target(capsys):
-    callback = MagicMock()
+    callback = Mock()
 
     with peek_stderr_write(callback) as t:
         t("foo")
 
     assert [call("foo")] == callback.call_args_list
-    assert [call()] == callback.__enter__.call_args_list
-    assert [call(None, None, None)] == callback.__exit__.call_args_list
 
     captured = capsys.readouterr()
     assert "foo" == captured.err
 
 
 def test_raise(capsys):
-    callback = MagicMock(side_effect=MockCallError)
+    callback = Mock(side_effect=MockError)
 
     with peek_stdout_write(callback):
-        with pytest.raises(MockCallError):
+        with pytest.raises(MockError):
             print("foo")
 
     print("bar")
 
     assert [call("foo")] == callback.call_args_list
-    assert [call()] == callback.__enter__.call_args_list
-    assert [call(None, None, None)] == callback.__exit__.call_args_list
 
     captured = capsys.readouterr()
     assert "bar\n" == captured.out
 
 
-def test_raise_close(capsys):
-    callback = MagicMock()
-    callback.__exit__ = Mock(side_effect=MockCloseError)
-
-    with pytest.raises(MockCloseError):
-        with peek_stdout_write(callback):
-            print("foo")
-
-    print("bar")
-
-    # with capsys.disabled():
-    #     print(callback.call_args_list)
-    #     print(callback.close.call_args_list)
-
-    assert [call("foo"), call("\n")] == callback.call_args_list
-    assert [call()] == callback.__enter__.call_args_list
-    assert [call(None, None, None)] == callback.__exit__.call_args_list
-
-    captured = capsys.readouterr()
-    assert "foo\nbar\n" == captured.out
-
-
-class MockCallError(BaseException):
-    pass
-
-
-class MockCloseError(BaseException):
+class MockError(BaseException):
     pass
