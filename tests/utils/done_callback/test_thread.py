2b14da7182411ce716d1773f1e3dab321f33c337
Tai Sakuma <tai.sakuma@gmail.com>
2022-06-07 19:09:41 -0400

    update tests

--- tests/utils/done_callback/test_thread.py
+++ tests/utils/done_callback/test_thread.py
@@ -32,10 +32,11 @@ def done():
     yield Done()
 
 
-def test_one(done: Done):
+def test_close(done: Done):
     obj = ThreadDoneCallback(done=done)
     t = ExcThread(target=target, args=(obj,))
     t.start()
+    time.sleep(0.005)
     obj.close()
     assert {t} == done.args
     t.join()
@@ -45,6 +46,7 @@ def test_with(done: Done):
     with ThreadDoneCallback(done=done) as obj:
         t = ExcThread(target=target, args=(obj,))
         t.start()
+        time.sleep(0.005)
     assert {t} == done.args
     t.join()
 
@@ -54,14 +56,15 @@ def test_register_arg(done: Done):
         delay = random.random() * 0.01
         time.sleep(delay)
 
-    obj = ThreadDoneCallback(done=done)
-    t = ExcThread(target=target)
+    with ThreadDoneCallback(done=done) as obj:
+        t = ExcThread(target=target)
 
-    # manually provide the thread object
-    assert t == obj.register(t)
+        # manually provide the thread object
+        assert t == obj.register(t)
 
-    t.start()
-    obj.close()
+        t.start()
+        time.sleep(0.005)
+
     assert {t} == done.args
     t.join()
 
@@ -71,20 +74,23 @@ def test_daemon(done: Done):
     obj = ThreadDoneCallback(done=done)
     t = ExcThread(target=target, args=(obj,))
     t.start()
+    time.sleep(0.005)
     # obj.close()  # not call closed()
     t.join()
 
 
-@pytest.mark.parametrize("nthreads", [0, 1, 2, 5, 10])
-def test_multiple(nthreads: int, done: Done):
+@pytest.mark.parametrize("n_threads", [0, 1, 2, 5, 10])
+def test_multiple(n_threads: int, done: Done):
 
-    obj = ThreadDoneCallback(done=done)
+    with ThreadDoneCallback(done=done) as obj:
 
-    threads = {ExcThread(target=target, args=(obj,)) for _ in range(nthreads)}
-    for t in threads:
-        t.start()
+        threads = {
+            ExcThread(target=target, args=(obj,)) for _ in range(n_threads)
+        }
+        for t in threads:
+            t.start()
+        time.sleep(0.005)
 
-    obj.close()
     assert threads == done.args
 
     for t in threads:
@@ -96,7 +102,7 @@ def test_raise_close_from_thread(done: Done):
         obj.register()
 
         obj.close()
-        # raised originally here, then reraised at the join() by
+        # raised originally here, then re-raised at the join() by
         # ExcThread in the main thread.
 
     obj = ThreadDoneCallback(done=done)
@@ -111,6 +117,7 @@ def test_raise_in_done():
     obj = ThreadDoneCallback(done=done)
     t = ExcThread(target=target, args=(obj,))
     t.start()
+    time.sleep(0.005)
 
     with pytest.raises(ValueError):
         obj.close()
@@ -123,6 +130,7 @@ def test_interval(done: Done):
     obj = ThreadDoneCallback(done=done, interval=interval)
     t = ExcThread(target=target, args=(obj,))
     t.start()
+    time.sleep(0.005)
 
     obj.close()
     t.join()
