f5ec607153122f221a58ebf1adde14e92c5eb08c
Tai Sakuma <tai.sakuma@gmail.com>
2022-06-13 13:42:53 -0400

    update tests

--- tests/process/trace/funcs.py
+++ tests/process/trace/funcs.py
@@ -1,11 +1,23 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
-from operator import itemgetter
+from operator import itemgetter, attrgetter
 from itertools import groupby
 from keyword import iskeyword
 
 from unittest.mock import Mock
 
-from typing import Optional, Any, List, Tuple, Set, Dict
+from typing import (
+    Iterable,
+    Iterator,
+    Mapping,
+    Optional,
+    Any,
+    List,
+    Tuple,
+    Set,
+    TypedDict,
+)
 from types import FrameType
 
 
@@ -24,44 +36,50 @@ class TraceSummary:
     opcode: TracedScope = field(default_factory=TracedScope)
 
 
+class _S(TypedDict):
+    event: str
+    module: str
+    func: str
+
+
 def summarize_trace_calls(
     mock_trace: Mock,
     modules: Optional[Set[str]] = None,
 ) -> TraceSummary:
     """Traced modules and functions for each event"""
 
-    args_ = trace_call_args(mock_trace)  # type: ignore
-    # [(frame, event, arg), ...]
+    args_ = trace_call_args(mock_trace)
+    # ((frame, event, arg), ...)
 
-    args = [
-        {
-            "event": event,
-            "module": frame.f_globals.get("__name__"),
-            "func": frame.f_code.co_name,
-        }
+    args = (
+        _S(
+            event=event,
+            module=frame.f_globals.get("__name__", ""),
+            func=frame.f_code.co_name,
+        )
         for frame, event, _ in args_
-    ]
+    )
     # [{"event": event, "module": module, "func": func}, ...]
 
     if modules is not None:
-        args = [d for d in args if d["module"] in modules]
+        args = (d for d in args if d["module"] in modules)
 
-    args = sorted(args, key=itemgetter("event"))
-    # sorted for groupby()
+    args_sorted = sorted(args, key=itemgetter("event"))
+    # sort for groupby()
 
     args_by_events = [
         (
             event + ("_" if iskeyword(event) else ""),  # "return_"
             list(args),  # expand to avoid exhaustion below
         )
-        for event, args in groupby(args, itemgetter("event"))
+        for event, args in groupby(args_sorted, itemgetter("event"))
     ]
 
     ret = TraceSummary(
         **{
             event: TracedScope(
                 **{
-                    scope: ordered_uniq_values(args, scope)
+                    scope: list(ordered_uniq_values(args, scope))  # type: ignore
                     for scope in ("module", "func")
                 }
             )
@@ -71,13 +89,15 @@ def summarize_trace_calls(
     return ret
 
 
-def ordered_uniq_values(dicts: List[Dict], key: Any) -> List:
+def ordered_uniq_values(
+    dicts: Iterable[Mapping[str, str]], key: str
+) -> Iterator[str]:
     """List of unique values of dicts for the key
 
     Note: The order is preserved while consecutive duplicates are removed.
     """
-    return [v for v, _ in groupby([d[key] for d in dicts])]
+    return (v for v, _ in groupby([d[key] for d in dicts]))
 
 
-def trace_call_args(trace: Mock) -> List[Tuple[FrameType, str, Any]]:
-    return [call.args for call in trace.call_args_list]
+def trace_call_args(trace: Mock) -> Iterator[Tuple[FrameType, str, Any]]:
+    return map(attrgetter("args"), trace.call_args_list)
