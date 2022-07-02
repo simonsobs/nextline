b0e9359c5d2cd00327a2aeb7ff32e57de2f97268
Tai Sakuma <tai.sakuma@gmail.com>
2022-07-02 16:07:28 -0400

    add wrap_futures_process_python_exit()

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
 import sys
+import atexit
+from concurrent.futures.process import _python_exit
 import threading
+from typing import Any, Callable, List
 import pytest
 from unittest.mock import Mock
 
@@ -21,3 +25,50 @@ def monkey_patch_trace(monkeypatch):
     mock_class = Mock(return_value=mock_instance)
     monkeypatch.setattr("nextline.process.run.Trace", mock_class)
     yield mock_class
+
+
+@pytest.fixture(scope="session", autouse=True)
+def wrap_futures_process_python_exit():
+    """Catch error at exit in Python 3.8
+
+    https://stackoverflow.com/q/68091084/7309855
+    """
+    yield
+    if sys.version_info >= (3, 9):
+        return
+
+    if not _atexit_is_registered(_python_exit):
+        return
+
+    atexit.unregister(_python_exit)
+    atexit.register(_wrap_python_exit)
+
+
+def _wrap_python_exit(*args, **kwargs):
+    try:
+        _python_exit(*args, **kwargs)
+    except OSError:
+        pass
+
+
+def _atexit_is_registered(func: Callable[..., Any]) -> bool:
+    return func in _atexit_registered()
+
+
+def _atexit_registered() -> List[Callable[..., Any]]:
+    # https://stackoverflow.com/a/63813607/7309855
+    import atexit
+
+    ret = []
+
+    class Capture:
+        def __eq__(self, other):
+            ret.append(other)
+            return False
+
+        def __call__(self):
+            pass
+
+    c = Capture()
+    atexit.unregister(c)
+    return ret
