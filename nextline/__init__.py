749a67afa36ad9ec5740f2765766e304a1e49dc5
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-13 15:52:22 -0500

    replace __version__ in __init__.py

--- nextline/__init__.py
+++ nextline/__init__.py
@@ -1,17 +1,5 @@
-__all__ = ["Nextline"]
+__all__ = ["Nextline", "__version__"]
 
 from .main import Nextline
 
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-"""str: version
-
-The version string, e.g., "0.1.2", "0.1.2+83.ga093a20.dirty".
-generated from git tags by versioneer.
-
-Versioneer: https://github.com/warner/python-versioneer
-
-"""
-
-del get_versions
+from nextline.__about__ import __version__
