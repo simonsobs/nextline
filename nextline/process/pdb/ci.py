c8af2044dc27a77d3c6a909e571df682fdd29acc
Tai Sakuma <tai.sakuma@gmail.com>
2023-01-13 19:37:08 -0500

    restore accidentally committed change

--- nextline/process/pdb/ci.py
+++ nextline/process/pdb/ci.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
-from logging import getLogger
-from queue import Queue
 from types import FrameType
-from typing import Any, Callable, Tuple
+from queue import Queue
+from typing import Callable, Tuple, Any
 
-from ...types import PromptNo, TraceNo
 from ..callback import Callback
+from ...types import TraceNo, PromptNo
 
 
 def pdb_command_interface(
@@ -39,10 +38,9 @@ def pdb_command_interface(
 
     def send_command(command: str, prompt_no: PromptNo) -> None:
         """send a command to pdb"""
-        # print(prompt_no)
-        logger = getLogger(__name__)
-        logger.debug(f"send_command: {command}")
-        callback.prompt_end(trace_no=trace_no, prompt_no=prompt_no, command=command)
+        callback.prompt_end(
+            trace_no=trace_no, prompt_no=prompt_no, command=command
+        )
         queue_stdin.put(command)
 
     return wait_prompt, send_command
