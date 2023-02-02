f38abf640249a7399aa7f884a90f2695394323e9
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-02 18:26:39 -0500

    add model.py and test_model.py

---
+++ nextline/model.py
@@ -0,0 +1,68 @@
+from typing import Any, Optional
+
+from transitions import EventData
+
+from .context import Context
+from .fsm import build_state_machine
+
+
+class Model:
+    def __init__(self, context: Context):
+        self._context = context
+
+        self._machine = build_state_machine(model=self)
+        self._machine.after_state_change = self.after_state_change.__name__  # type: ignore
+
+        assert self.state  # type: ignore
+
+    def __repr__(self):
+        # e.g., "<Model 'running'>"
+        return f'<{self.__class__.__name__} {self.state!r}>'
+
+    async def after_state_change(self, _: EventData) -> None:
+        await self._context.state_change(self.state)  # type: ignore
+
+    async def on_enter_initialized(self, _: EventData) -> None:
+        await self._context.initialize()
+
+    async def on_enter_running(self, _: EventData) -> None:
+        await self._context.run()
+
+    def interrupt(self) -> None:
+        assert self.is_running()  # type: ignore
+        self._context.interrupt()
+
+    def terminate(self) -> None:
+        assert self.is_running()  # type: ignore
+        self._context.terminate()
+
+    def kill(self) -> None:
+        assert self.is_running()  # type: ignore
+        self._context.kill()
+
+    async def on_enter_finished(self, _: EventData) -> None:
+        await self._context.finish()
+
+    def exception(self) -> Optional[BaseException]:
+        assert self.is_finished()  # type: ignore
+        return self._context.exception
+
+    def result(self) -> Any:
+        assert self.is_finished()  # type: ignore
+        if exc := self._context.exception:
+            raise exc
+        return self._context.result
+
+    async def on_enter_closed(self, _: EventData) -> None:
+        await self._context.close()
+
+    async def on_reset(self, event: EventData) -> None:
+        await self._context.reset(*event.args, **event.kwargs)
+
+    async def __aenter__(self):
+        await self.start()
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        del exc_type, exc_value, traceback
+        await self.close()
---
+++ tests/test_model.py
@@ -0,0 +1,153 @@
+from unittest.mock import Mock, call
+
+import pytest
+
+from nextline.context import Context
+from nextline.model import Model
+
+
+def test_repr(context: Context) -> None:
+    model = Model(context=context)
+    assert repr(model)
+
+
+async def test_callbacks_transitions(context: Mock) -> None:
+    model = Model(context=context)
+
+    async with model:
+
+        # initialized -- reset() --> initialized
+
+        assert model.is_initialized()  # type: ignore
+        expected_calls = [call.initialize(), call.state_change('initialized')]
+        assert expected_calls == context.method_calls
+        context.reset_mock()
+
+        await model.reset(10, foo='bar')  # type: ignore
+        assert model.is_initialized()  # type: ignore
+        expected_calls = [
+            call.reset(10, foo='bar'),
+            call.initialize(),
+            call.state_change('initialized'),
+        ]
+        assert expected_calls == context.method_calls
+        context.reset_mock()
+
+        # initialized -- run() --> running -- finish() --> finished -- reset() --> initialized
+
+        await model.run()  # type: ignore
+        assert model.is_running()  # type: ignore
+        expected_calls = [call.run(), call.state_change('running')]
+        assert expected_calls == context.method_calls
+        context.reset_mock()
+
+        await model.finish()  # type: ignore
+        assert model.is_finished()  # type: ignore
+        expected_calls = [call.finish(), call.state_change('finished')]
+        assert expected_calls == context.method_calls
+        context.reset_mock()
+
+        await model.reset(10, foo='bar')  # type: ignore
+        assert model.is_initialized()  # type: ignore
+        expected_calls = [
+            call.reset(10, foo='bar'),
+            call.initialize(),
+            call.state_change('initialized'),
+        ]
+        assert expected_calls == context.method_calls
+        context.reset_mock()
+
+    assert model.is_closed()  # type: ignore
+    expected_calls = [call.close(), call.state_change('closed')]
+    assert expected_calls == context.method_calls
+
+
+async def test_signals(context: Mock) -> None:
+    model = Model(context=context)
+
+    await model.to_running()  # type: ignore
+    assert model.is_running()  # type: ignore
+
+    context.reset_mock()
+
+    model.interrupt()
+    expected_calls = [call.interrupt()]
+    assert expected_calls == context.method_calls
+    context.reset_mock()
+
+    model.terminate()
+    expected_calls = [call.terminate()]
+    assert expected_calls == context.method_calls
+    context.reset_mock()
+
+    model.kill()
+    expected_calls = [call.kill()]
+    assert expected_calls == context.method_calls
+    context.reset_mock()
+
+
+async def test_signals_raised(context: Mock) -> None:
+    model = Model(context=context)
+
+    for state in model._machine.states:
+        if state == 'running':
+            continue
+        model._machine.set_state(state)
+        assert model.state == state  # type: ignore
+
+        with pytest.raises(AssertionError):
+            model.interrupt()
+
+        with pytest.raises(AssertionError):
+            model.terminate()
+
+        with pytest.raises(AssertionError):
+            model.kill()
+
+
+class MockException(BaseException):
+    pass
+
+
+async def test_results(context: Mock) -> None:
+    model = Model(context=context)
+
+    await model.to_finished()  # type: ignore
+    assert model.is_finished()  # type: ignore
+
+    context.reset_mock()
+
+    exc = MockException()
+    context.exception = exc
+    assert exc == model.exception()
+
+    with pytest.raises(MockException):
+        model.result()
+
+    context.exception = None
+    result = object()
+    context.result = result
+    assert result == model.result()
+
+
+async def test_results_raised(context: Mock) -> None:
+    model = Model(context=context)
+
+    for state in model._machine.states:
+        if state == 'finished':
+            continue
+        model._machine.set_state(state)
+        assert model.state == state  # type: ignore
+
+        with pytest.raises(AssertionError):
+            model.exception()
+
+        with pytest.raises(AssertionError):
+            model.result()
+
+
+@pytest.fixture
+def context() -> Mock:
+    y = Mock(spec=Context)
+    y.exception = None
+    return y
