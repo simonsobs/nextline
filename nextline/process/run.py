f726a15e0a1e879bf68e4e9bf109c5ddf26b5ddd
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-03 17:49:32 -0500

    add docstrings, reorder objects

--- nextline/process/run.py
+++ nextline/process/run.py
@@ -35,6 +35,7 @@ _q_registry: QueueRegistry | None = None
 
 
 def set_queues(q_commands: QueueCommands, q_registry: QueueRegistry) -> None:
+    '''Initializer of ProcessPoolExecutor that receives the queues.'''
     global _q_commands, _q_registry
     _q_commands = q_commands
     _q_registry = q_registry
@@ -46,16 +47,17 @@ class RunArg(TypedDict):
     filename: str
 
 
+def main(run_arg: RunArg) -> Tuple[Any, BaseException | None]:
+    '''The entry point in the process in which the script is executed.'''
+    assert _q_registry
+    assert _q_commands
+    return run_(run_arg, _q_commands, _q_registry)
+
+
 class Context(TypedDict):
     callback: Callback
     pdb_ci_map: PdbCiMap
     modules_to_trace: Set[str]
-
-
-def main(run_arg: RunArg) -> Tuple[Any, BaseException | None]:
-    assert _q_registry
-    assert _q_commands
-    return run_(run_arg, _q_commands, _q_registry)
 
 
 def run_(
