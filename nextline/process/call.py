a73e0cc3abba99d87db9c10ce8e4442d8ec6b7fc
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-06 15:45:28 -0500

    extract sys_trace()

--- nextline/process/call.py
+++ nextline/process/call.py
@@ -2,12 +2,56 @@ from __future__ import annotations
 
 import sys
 import threading
+from contextlib import contextmanager
 from typing import TYPE_CHECKING, Callable, Optional, Tuple, TypeVar
+
+from nextline.process.trace import TraceSkipModule
 
 if TYPE_CHECKING:
     from nextline.types import TraceFunc
 
 T = TypeVar("T")
+
+
+@contextmanager
+def sys_trace(trace_func: TraceFunc, thread: Optional[bool] = True):
+    '''Trace callables in the context and all threads created during the context.
+
+    Notes
+    -----
+    All new threads created during the context are traced regardless of whether
+    the threads are created by code in the context. If the thread option is
+    false, no new threads will be traced.
+
+    Example:
+
+    >>> def trace_func(frame, event, arg):
+    ...     # will be skipped in the doctest because of TraceSkipModule
+    ...     return trace_func
+
+    >>> def callable():
+    ...     pass
+
+    >>> with sys_trace(trace_func):
+    ...     callable()
+
+    '''
+    trace_func_org = sys.gettrace()
+
+    if thread:
+        threading.settrace(trace_func)
+
+    # Skip the context manager and this module otherwise they will be traced after
+    # the yield is returned.
+    trace_func = TraceSkipModule(trace_func, {contextmanager.__module__, __name__})
+    sys.settrace(trace_func)
+
+    try:
+        yield
+    finally:
+        sys.settrace(trace_func_org)
+        if thread:
+            threading.settrace(trace_func_org)  # type: ignore
 
 
 def call_with_trace(
@@ -46,28 +90,20 @@ def call_with_trace(
     ret = None
     exc = None
 
-    trace_org = sys.gettrace()
-    if thread:
-        threading.settrace(trace)
-    sys.settrace(trace)
-    try:
-        ret = func()
-    except BaseException as e:
-        exc = e
-    finally:
-        sys.settrace(trace_org)
-        if thread:
-            threading.settrace(trace_org)  # type: ignore
+    with sys_trace(trace, thread):
+        try:
+            ret = func()
+        except BaseException as e:
+            exc = e
+    if exc:
 
-        if exc:
+        # How to print the exception in the same way as the interpreter.
+        # import traceback
+        # traceback.print_exception(type(exc), exc, exc.__traceback__)
 
-            # How to print the exception in the same way as the interpreter.
-            # import traceback
-            # traceback.print_exception(type(exc), exc, exc.__traceback__)
+        if exc.__traceback__:
+            # remove this frame from the traceback.
+            # Note: exc.__traceback__ is sys._getframe()
+            exc.__traceback__ = exc.__traceback__.tb_next
 
-            if exc.__traceback__:
-                # remove this frame from the traceback.
-                # Note: exc.__traceback__ is sys._getframe()
-                exc.__traceback__ = exc.__traceback__.tb_next
-
-        return ret, exc
+    return ret, exc
