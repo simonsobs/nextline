22ded2ffc7afd04e66e74188a64c46d53931120d
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-06 16:32:08 -0500

    remove call_with_trace()

--- nextline/process/call.py
+++ nextline/process/call.py
@@ -3,14 +3,12 @@ from __future__ import annotations
 import sys
 import threading
 from contextlib import contextmanager
-from typing import TYPE_CHECKING, Callable, Optional, Tuple, TypeVar
+from typing import TYPE_CHECKING, Optional
 
 from nextline.process.trace import TraceSkipModule
 
 if TYPE_CHECKING:
     from nextline.types import TraceFunc
-
-T = TypeVar("T")
 
 
 @contextmanager
@@ -52,58 +50,3 @@ def sys_trace(trace_func: TraceFunc, thread: Optional[bool] = True):
         sys.settrace(trace_func_org)
         if thread:
             threading.settrace(trace_func_org)  # type: ignore
-
-
-def call_with_trace(
-    func: Callable[[], T | None],
-    trace: TraceFunc,
-    thread: Optional[bool] = True,
-) -> Tuple[T | None, BaseException | None]:
-    """Set the trace function while running the function
-
-    Notes
-    -----
-    The trace function will be used in all new threads created during the
-    function execution regardless of whether the threads are created by the
-    function. If the thread option is false, the trace function will not be use
-    in any new threads.
-
-    Parameters
-    ----------
-    func : callable
-        A function to be called without any args. Use functools.partial to
-        provide args.
-    trace: callable
-        A trace function.
-    thread: bool, default True
-        If False, no new threads will be traced.
-
-    Returns
-    -------
-    tuple
-        A tuple with two elements. The first is the return value of func() or
-        None if an exception is raised in func(). The second is the exception
-        raised in func() or None if no exception is rased.
-
-    """
-
-    ret = None
-    exc = None
-
-    with sys_trace(trace, thread):
-        try:
-            ret = func()
-        except BaseException as e:
-            exc = e
-    if exc:
-
-        # How to print the exception in the same way as the interpreter.
-        # import traceback
-        # traceback.print_exception(type(exc), exc, exc.__traceback__)
-
-        if exc.__traceback__:
-            # remove this frame from the traceback.
-            # Note: exc.__traceback__ is sys._getframe()
-            exc.__traceback__ = exc.__traceback__.tb_next
-
-    return ret, exc
