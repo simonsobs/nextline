6ddcae714e5b5bfdc5daac541739b2d4aa831ed4
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-16 10:53:38 -0500

    replace prompt_start() with PromptInfoRegistrar

--- nextline/process/callback.py
+++ nextline/process/callback.py
@@ -130,46 +130,58 @@ def trace_call(
         registrar.put_prompt_info_for_trace(trace_no, prompt_info)
 
 
-def prompt_start(
-    run_no: RunNo,
-    trace_no: TraceNo,
-    prompt_no: PromptNo,
-    registrar: RegistrarProxy,
-    trace_args: TraceArgs,
-    out: str,
-    last_prompt_frame_map: Dict[TraceNo, FrameType],
-) -> PromptEnd:
+class PromptInfoRegistrar:
+    def __init__(self, run_no: RunNo, registrar: RegistrarProxy):
+        self._run_no = run_no
+        self._registrar = registrar
+        self._end_map: Dict[PromptNo, PromptEnd] = {}
 
-    frame, event, _ = trace_args
-    file_name = _to_canonic(frame.f_code.co_filename)
-    line_no = frame.f_lineno
-    prompt_info = PromptInfo(
-        run_no=run_no,
-        trace_no=trace_no,
-        prompt_no=prompt_no,
-        open=True,
-        event=event,
-        file_name=file_name,
-        line_no=line_no,
-        stdout=out,
-        started_at=datetime.datetime.utcnow(),
-    )
-    registrar.put_prompt_info(prompt_info)
-    registrar.put_prompt_info_for_trace(trace_no, prompt_info)
+    def prompt_start(
+        self,
+        trace_no: TraceNo,
+        prompt_no: PromptNo,
+        trace_args: TraceArgs,
+        out: str,
+        last_prompt_frame_map: Dict[TraceNo, FrameType],
+    ) -> None:
+        def _prompt() -> PromptEnd:
 
-    last_prompt_frame_map[trace_no] = frame
+            frame, event, _ = trace_args
+            file_name = _to_canonic(frame.f_code.co_filename)
+            line_no = frame.f_lineno
+            prompt_info = PromptInfo(
+                run_no=self._run_no,
+                trace_no=trace_no,
+                prompt_no=prompt_no,
+                open=True,
+                event=event,
+                file_name=file_name,
+                line_no=line_no,
+                stdout=out,
+                started_at=datetime.datetime.utcnow(),
+            )
+            self._registrar.put_prompt_info(prompt_info)
+            self._registrar.put_prompt_info_for_trace(trace_no, prompt_info)
 
-    def prompt_end(command: str) -> None:
-        prompt_info_end = dataclasses.replace(
-            prompt_info,
-            open=False,
-            command=command,
-            ended_at=datetime.datetime.utcnow(),
-        )
-        registrar.put_prompt_info(prompt_info_end)
-        registrar.put_prompt_info_for_trace(trace_no, prompt_info_end)
+            last_prompt_frame_map[trace_no] = frame
 
-    return prompt_end
+            def _end(command: str) -> None:
+                prompt_info_end = dataclasses.replace(
+                    prompt_info,
+                    open=False,
+                    command=command,
+                    ended_at=datetime.datetime.utcnow(),
+                )
+                self._registrar.put_prompt_info(prompt_info_end)
+                self._registrar.put_prompt_info_for_trace(trace_no, prompt_info_end)
+
+            return _end
+
+        self._end_map[prompt_no] = _prompt()
+
+    def prompt_end(self, trace_no: TraceNo, prompt_no: PromptNo, command: str) -> None:
+        end = self._end_map.pop(prompt_no)
+        end(command)
 
 
 class AddModuleToTrace:
@@ -204,7 +216,9 @@ class Callback:
         self._entering_thread: Optional[Thread] = None
         self._last_prompt_frame_map: Dict[TraceNo, FrameType] = {}
         self._trace_end_map: Dict[TraceNo, TraceEnd] = {}
-        self._prompt_end_map: Dict[PromptNo, PromptEnd] = {}
+        self._prompt_info_registrar = PromptInfoRegistrar(
+            run_no=run_no, registrar=registrar
+        )
 
     def task_or_thread_start(self, trace_no: TraceNo) -> None:
         task_or_thread = current_task_or_thread()
@@ -268,21 +282,22 @@ class Callback:
         out: str,
     ) -> None:
 
-        prompt_end = prompt_start(
-            run_no=self._run_no,
+        self._prompt_info_registrar.prompt_start(
             trace_no=trace_no,
             prompt_no=prompt_no,
-            registrar=self._registrar,
             trace_args=trace_args,
             out=out,
             last_prompt_frame_map=self._last_prompt_frame_map,
         )
-        self._prompt_end_map[prompt_no] = prompt_end
+
         self._add_module_to_trace.prompt_start(trace_args)
 
     def prompt_end(self, trace_no: TraceNo, prompt_no: PromptNo, command: str) -> None:
-        prompt_end = self._prompt_end_map.pop(prompt_no)
-        prompt_end(command)
+        self._prompt_info_registrar.prompt_end(
+            trace_no=trace_no,
+            prompt_no=prompt_no,
+            command=command,
+        )
 
     def stdout(self, task_or_thread: Task | Thread, line: str):
         trace_no = self._trace_no_map[task_or_thread]
