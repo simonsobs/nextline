ba7b1e36b22d1ed31b79c00b0dbdbd150159f0bc
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-22 15:53:46 -0500

    use stdin.readline() instead of string matching to detect end of stdout

--- nextline/process/pdb/stream.py
+++ nextline/process/pdb/stream.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from io import TextIOWrapper
 from queue import Queue
-from typing import Generator
+from typing import Generator, Literal
 
 
 class StreamOut(TextIOWrapper):
@@ -18,31 +18,36 @@ class StreamOut(TextIOWrapper):
 
 
 class StreamIn(TextIOWrapper):
-    def __init__(self, queue: Queue[str]):
+    def __init__(self, queue: Queue[str], cli: CmdLoopInterface):
         self.queue = queue
+        self._cli = cli
 
     def readline(self):
+        self._cli.prompt()
         return self.queue.get()
 
 
 class CmdLoopInterface:
     def __init__(self) -> None:
-        self._prompt_end = '(Pdb) '
 
         self._queue_stdin: Queue[str] = Queue()
-        self._queue_stdout: Queue[str | None] = Queue()
+        self._queue_stdout: Queue[str | None | Literal[True]] = Queue()
 
-        self.stdin = StreamIn(self._queue_stdin)
+        self.stdin = StreamIn(self._queue_stdin, self)
         self.stdout = StreamOut(self._queue_stdout)  # type: ignore
 
     def prompts(self) -> Generator[str, str, None]:
-        '''Yield each Pdb prompt from stdout.'''
+        '''Yield each prompt from stdout.'''
         prompt = ''
         while (msg := self._queue_stdout.get()) is not None:
-            prompt += msg
-            if self._prompt_end == msg:  # '(Pdb) '
+            if msg is True:
                 yield prompt
                 prompt = ''
+                continue
+            prompt += msg
+
+    def prompt(self) -> None:
+        self._queue_stdout.put(True)
 
     def issue(self, command: str) -> None:
         self._queue_stdin.put(command)
