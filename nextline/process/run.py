65b9954df8c56ce55ad64eb88a357fd6c836c19b
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-23 10:15:18 -0500

    remove a ThreadPoolExecutor

--- nextline/process/run.py
+++ nextline/process/run.py
@@ -24,7 +24,6 @@ class TraceContext(TypedDict):
     modules_to_trace: Set[str]
     trace_no_counter: Callable[[], TraceNo]
     prompt_no_counter: Callable[[], PromptNo]
-    executor: ThreadPoolExecutor
 
 
 def run_(
@@ -87,21 +86,18 @@ def _trace(run_no: RunNo, q_commands: QueueCommands, q_registry: QueueRegistry):
         modules_to_trace=modules_to_trace,
     ) as callback:
 
-        with ThreadPoolExecutor() as executor:
+        context = TraceContext(
+            callback=callback,
+            pdb_ci_map=pdb_ci_map,
+            modules_to_trace=modules_to_trace,
+            trace_no_counter=TraceNoCounter(1),
+            prompt_no_counter=PromptNoCounter(1),
+        )
 
-            context = TraceContext(
-                callback=callback,
-                pdb_ci_map=pdb_ci_map,
-                modules_to_trace=modules_to_trace,
-                trace_no_counter=TraceNoCounter(1),
-                prompt_no_counter=PromptNoCounter(1),
-                executor=executor,
-            )
+        trace = Trace(context=context)
 
-            trace = Trace(context=context)
-
-            with relay_commands(q_commands, pdb_ci_map):
-                yield trace
+        with relay_commands(q_commands, pdb_ci_map):
+            yield trace
 
 
 def _compile(code: CodeType | str, filename: str) -> CodeType:
