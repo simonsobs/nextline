740be90d713d81185838b68d0f0433d46624edf1
Tai Sakuma <tai.sakuma@gmail.com>
2023-02-24 11:15:52 -0500

    add a comment about the nosigint (No SIGINT) option of Pdb

--- nextline/process/pdb/custom.py
+++ nextline/process/pdb/custom.py
@@ -17,6 +17,21 @@ class CustomizedPdb(Pdb):
         stdout: IO[str],
     ):
         super().__init__(stdin=stdin, stdout=stdout, nosigint=True, readrc=False)
+        # NOTE: nosigint (No SIGINT) is False by default.  When False, Pdb lets
+        # the user to break into a debugger again with SIGINT after the
+        # `continue` command is issued. This feature is potentially useful in
+        # Nextline.
+        #
+        #  However, it is turned off now for two reasons:
+        #
+        #  1. Pdb calls `sys.settrace(None)` when the `continue` command is
+        #     issued and `sys.settrace(pdb.trace_dispatch)` when SIGINT is
+        #     handled, which removes Nextline.  We will need to override Pdb
+        #     methods to set Nextline back.
+        #
+        #  2. Nextline uses multiple instances of Pdb.  We will need to break
+        #     into the right instance of Pdb when SIGINT is handled.
+
         self._cmdloop_hook = cmdloop_hook
 
         # self.quitting = True # not sure if necessary
