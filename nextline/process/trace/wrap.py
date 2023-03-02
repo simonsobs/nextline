584af3413b8552932afafd5623e61e36a8bc4450
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-02 13:36:44 -0500

    delete Filter(), DispatchForThreadOrTask(), and FromFactory()

--- nextline/process/trace/wrap.py
+++ nextline/process/trace/wrap.py
@@ -1,63 +1,10 @@
 from __future__ import annotations
 
-from asyncio import Task
-from logging import getLogger
-from threading import Thread
 from types import FrameType
 from typing import TYPE_CHECKING, Any, Callable, ContextManager, Optional
-from weakref import WeakKeyDictionary
-
-from nextline.utils import current_task_or_thread
 
 if TYPE_CHECKING:
     from sys import TraceFunction as TraceFunc  # type: ignore  # noqa: F401
-
-
-def Filter(
-    trace: TraceFunc, filter: Callable[[FrameType, str, Any], bool]
-) -> TraceFunc:
-    '''Skip if the filter returns False.'''
-
-    def _trace(frame: FrameType, event, arg) -> Optional[TraceFunc]:
-        if filter(frame, event, arg):
-            return trace(frame, event, arg)
-        return None
-
-    return _trace
-
-
-def DispatchForThreadOrTask(factory: Callable[[], TraceFunc]) -> TraceFunc:
-    '''Create a new trace function for each thread or asyncio task.'''
-
-    map: WeakKeyDictionary[Task | Thread, TraceFunc] = WeakKeyDictionary()
-
-    def ret(frame, event, arg) -> Optional[TraceFunc]:
-        key = current_task_or_thread()
-        trace = map.get(key)
-        if not trace:
-            trace = factory()
-            map[key] = trace
-            logger = getLogger(__name__)
-            msg = f'{DispatchForThreadOrTask.__name__}: created trace for {key}'
-            logger.info(msg)
-        return trace(frame, event, arg)
-
-    return ret
-
-
-def FromFactory(factory: Callable[[], TraceFunc]) -> TraceFunc:
-    '''Create a trace function first time called.
-
-    Used to defer the creation of the trace function until need to call it.
-    '''
-    trace: TraceFunc | None = None
-
-    def global_trace(frame, event, arg) -> Optional[TraceFunc]:
-        nonlocal trace
-        trace = trace or factory()
-        return trace(frame, event, arg)
-
-    return global_trace
 
 
 def WithContext(
