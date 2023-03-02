2435e6e4e564d1a1ad8920c0690e3ad47a8fa416
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-02 16:12:51 -0500

    merge wrap.py into proxy.py

--- nextline/process/pdb/proxy.py
+++ nextline/process/pdb/proxy.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
 from contextlib import contextmanager
-from typing import TYPE_CHECKING
-
-from nextline.process.trace.wrap import WithContext
+from types import FrameType
+from typing import TYPE_CHECKING, Any, Callable, ContextManager, Optional
 
 from .custom import CustomizedPdb
 from .stream import StdInOut
@@ -21,6 +20,28 @@ def TraceCallCallback(trace: TraceFunc, callback: CallbackForTrace) -> TraceFunc
             yield
 
     return WithContext(trace, context=_context)
+
+
+def WithContext(
+    trace: TraceFunc, context: Callable[[FrameType, str, Any], ContextManager[None]]
+) -> TraceFunc:
+    def _create_local_trace() -> TraceFunc:
+        next_trace: TraceFunc | None = trace
+
+        def _local_trace(frame, event, arg) -> Optional[TraceFunc]:
+            nonlocal next_trace
+            assert next_trace
+            with context(frame, event, arg):
+                if next_trace := next_trace(frame, event, arg):
+                    return _local_trace
+                return None
+
+        return _local_trace
+
+    def _global_trace(frame: FrameType, event, arg) -> Optional[TraceFunc]:
+        return _create_local_trace()(frame, event, arg)
+
+    return _global_trace
 
 
 def instantiate_pdb(callback: CallbackForTrace):
