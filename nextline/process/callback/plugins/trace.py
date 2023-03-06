fd5af4d75d482f9c359ea6df1f596bd61266d9e7
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-06 13:25:18 -0500

    merge hook impls on TaskOrThreadToTraceMapper into private methods

--- nextline/process/callback/plugins/trace.py
+++ nextline/process/callback/plugins/trace.py
@@ -158,24 +158,20 @@ class TaskOrThreadToTraceMapper:
         return trace
 
     def _task_or_thread_start(self) -> None:
-        trace_no = self._trace_no_counter()
-        self._hook.hook.task_or_thread_start(trace_no=trace_no)
-
-    def _task_or_thread_end(self, task_or_thread: Task | Thread):
-        self._hook.hook.task_or_thread_end(task_or_thread=task_or_thread)
-
-    @hookimpl
-    def task_or_thread_start(self, trace_no: TraceNo) -> None:
         task_or_thread = current_task_or_thread()
+
+        trace_no = self._trace_no_counter()
         self._trace_no_map[task_or_thread] = trace_no
 
         if task_or_thread is not self._entering_thread:
             self._thread_task_done_callback.register(task_or_thread)
 
+        self._hook.hook.task_or_thread_start(trace_no=trace_no)
+
         self._trace_start(trace_no)
 
-    @hookimpl
-    def task_or_thread_end(self, task_or_thread: Task | Thread):
+    def _task_or_thread_end(self, task_or_thread: Task | Thread):
+        self._hook.hook.task_or_thread_end(task_or_thread=task_or_thread)
         trace_no = self._trace_no_map[task_or_thread]
         self._trace_end(trace_no)
 
