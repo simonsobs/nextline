631e3ddd517de3ed2fa839569497b59c7f62c515
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-14 10:37:46 -0400

    add hook implementations to Repeater

--- nextline/spawned/trace/plugins/repeat.py
+++ nextline/spawned/trace/plugins/repeat.py
@@ -1,16 +1,144 @@
+import datetime
+import os
+from typing import Callable, Dict, Generator
+
 from apluggy import PluginManager, contextmanager
 
 from nextline.spawned.trace.spec import hookimpl
-from nextline.spawned.types import OnStartTrace, QueueOut
-from nextline.types import TraceNo
+from nextline.spawned.trace.types import TraceArgs
+from nextline.spawned.types import (
+    OnEndCmdloop,
+    OnEndPrompt,
+    OnEndTrace,
+    OnEndTraceCall,
+    OnStartCmdloop,
+    OnStartPrompt,
+    OnStartTrace,
+    OnStartTraceCall,
+    OnWriteStdout,
+    QueueOut,
+)
+from nextline.types import PromptNo, TraceNo
 
 
 class Repeater:
     @hookimpl
-    def init(self, queue_out: QueueOut):
+    def init(self, hook: PluginManager, queue_out: QueueOut):
+        self._hook = hook
         self._queue_out = queue_out
 
     @hookimpl
     def on_start_trace(self, trace_no: TraceNo):
-        event = OnStartTrace(trace_no=trace_no)
+        assert trace_no == self._hook.hook.current_trace_no()
+        started_at = datetime.datetime.utcnow()
+        thread_no = self._hook.hook.current_thread_no()
+        task_no = self._hook.hook.current_task_no()
+        event = OnStartTrace(
+            started_at=started_at,
+            trace_no=trace_no,
+            thread_no=thread_no,
+            task_no=task_no,
+        )
         self._queue_out.put(event)
+
+    @hookimpl
+    def on_end_trace(self, trace_no: TraceNo) -> None:
+        ended_at = datetime.datetime.utcnow()
+        event = OnEndTrace(ended_at=ended_at, trace_no=trace_no)
+        self._queue_out.put(event)
+
+    @hookimpl
+    @contextmanager
+    def on_trace_call(self, trace_args: TraceArgs):
+        started_at = datetime.datetime.utcnow()
+        trace_no = self._hook.hook.current_trace_no()
+        frame, call_event, call_arg = trace_args
+        file_name = _to_canonic(frame.f_code.co_filename)
+        line_no = frame.f_lineno
+        event_start = OnStartTraceCall(
+            started_at=started_at,
+            trace_no=trace_no,
+            file_name=file_name,
+            line_no=line_no,
+            frame_object_id=id(frame),
+            call_event=call_event,
+        )
+        self._queue_out.put(event_start)
+
+        yield
+
+        ended_at = datetime.datetime.utcnow()
+        event_end = OnEndTraceCall(ended_at=ended_at, trace_no=trace_no)
+        self._queue_out.put(event_end)
+
+    @hookimpl
+    @contextmanager
+    def on_cmdloop(self) -> Generator[None, None, None]:
+        started_at = datetime.datetime.utcnow()
+        trace_no = self._hook.hook.current_trace_no()
+        event_start = OnStartCmdloop(started_at=started_at, trace_no=trace_no)
+        self._queue_out.put(event_start)
+
+        yield
+
+        ended_at = datetime.datetime.utcnow()
+        event_end = OnEndCmdloop(ended_at=ended_at, trace_no=trace_no)
+        self._queue_out.put(event_end)
+
+    @hookimpl
+    @contextmanager
+    def on_prompt(self, prompt_no: PromptNo, text: str) -> Generator[None, str, None]:
+        started_at = datetime.datetime.utcnow()
+        trace_no = self._hook.hook.current_trace_no()
+        event_start = OnStartPrompt(
+            started_at=started_at,
+            trace_no=trace_no,
+            prompt_no=prompt_no,
+            prompt_text=text,
+        )
+        self._queue_out.put(event_start)
+
+        command = yield
+        yield
+
+        ended_at = datetime.datetime.utcnow()
+        event_end = OnEndPrompt(
+            ended_at=ended_at,
+            trace_no=trace_no,
+            prompt_no=prompt_no,
+            command=command,
+        )
+        self._queue_out.put(event_end)
+
+    @hookimpl
+    def on_write_stdout(self, trace_no: TraceNo, line: str):
+        written_at = datetime.datetime.utcnow()
+        trace_no = self._hook.hook.current_trace_no()
+        event = OnWriteStdout(
+            written_at=written_at,
+            trace_no=trace_no,
+            text=line,
+        )
+        self._queue_out.put(event)
+
+
+def ToCanonic() -> Callable[[str], str]:
+    # Based on Bdb.canonic()
+    # https://github.com/python/cpython/blob/v3.10.5/Lib/bdb.py#L39-L54
+
+    cache: Dict[str, str] = {}
+
+    def to_canonic(filename: str) -> str:
+        if filename == "<" + filename[1:-1] + ">":
+            return filename
+        canonic = cache.get(filename)
+        if not canonic:
+            canonic = os.path.abspath(filename)
+            canonic = os.path.normcase(canonic)
+            cache[filename] = canonic
+        return canonic
+
+    return to_canonic
+
+
+_to_canonic = ToCanonic()
