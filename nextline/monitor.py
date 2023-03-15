05655706c86e272673dfc9281d65eabe945c8760
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-15 11:19:01 -0400

    clean code

--- nextline/monitor.py
+++ nextline/monitor.py
@@ -3,18 +3,7 @@ from logging import getLogger
 
 from apluggy import PluginManager
 
-from nextline.spawned import (
-    Event,
-    OnEndCmdloop,
-    OnEndPrompt,
-    OnEndTrace,
-    OnEndTraceCall,
-    OnStartCmdloop,
-    OnStartPrompt,
-    OnStartTrace,
-    OnStartTraceCall,
-    OnWriteStdout,
-)
+from nextline import spawned
 
 from .spawned import QueueOut
 from .utils import to_thread
@@ -39,24 +28,24 @@ class Monitor:
         while (event := await to_thread(self._queue.get)) is not None:
             await self._on_event(event)
 
-    async def _on_event(self, event: Event) -> None:
-        if isinstance(event, OnStartTrace):
+    async def _on_event(self, event: spawned.Event) -> None:
+        if isinstance(event, spawned.OnStartTrace):
             await self._hook.ahook.on_start_trace(event=event)
-        elif isinstance(event, OnEndTrace):
+        elif isinstance(event, spawned.OnEndTrace):
             await self._hook.ahook.on_end_trace(event=event)
-        elif isinstance(event, OnStartTraceCall):
+        elif isinstance(event, spawned.OnStartTraceCall):
             await self._hook.ahook.on_start_trace_call(event=event)
-        elif isinstance(event, OnEndTraceCall):
+        elif isinstance(event, spawned.OnEndTraceCall):
             await self._hook.ahook.on_end_trace_call(event=event)
-        elif isinstance(event, OnStartCmdloop):
+        elif isinstance(event, spawned.OnStartCmdloop):
             await self._hook.ahook.on_start_cmdloop(event=event)
-        elif isinstance(event, OnEndCmdloop):
+        elif isinstance(event, spawned.OnEndCmdloop):
             await self._hook.ahook.on_end_cmdloop(event=event)
-        elif isinstance(event, OnStartPrompt):
+        elif isinstance(event, spawned.OnStartPrompt):
             await self._hook.ahook.on_start_prompt(event=event)
-        elif isinstance(event, OnEndPrompt):
+        elif isinstance(event, spawned.OnEndPrompt):
             await self._hook.ahook.on_end_prompt(event=event)
-        elif isinstance(event, OnWriteStdout):
+        elif isinstance(event, spawned.OnWriteStdout):
             await self._hook.ahook.on_write_stdout(event=event)
         else:
             self._logger.warning(f'Unknown event: {event!r}')
