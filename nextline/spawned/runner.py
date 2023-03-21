7682ec7f2ef86646b59774418b7c00eb13d30cc3
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-21 11:23:32 -0400

    extract _remove_frame()

--- nextline/spawned/runner.py
+++ nextline/spawned/runner.py
@@ -1,5 +1,6 @@
 import inspect
-from logging import getLogger
+from types import FrameType
+from typing import Optional
 
 from .call import sys_trace
 from .plugin import build_hook
@@ -7,8 +8,6 @@ from .types import QueueIn, QueueOut, RunArg, RunResult
 
 
 def run(run_arg: RunArg, queue_in: QueueIn, queue_out: QueueOut) -> RunResult:
-
-    logger = getLogger(__name__)
 
     hook = build_hook(run_arg=run_arg, queue_in=queue_in, queue_out=queue_out)
 
@@ -28,15 +27,14 @@ def run(run_arg: RunArg, queue_in: QueueIn, queue_out: QueueOut) -> RunResult:
             with sys_trace(trace_func=trace_func):
                 result.ret = func()
         except BaseException as exc:
-            if exc.__traceback__:
-                # remove this frame from the traceback.
-                if exc.__traceback__.tb_frame is inspect.currentframe():
-                    exc.__traceback__ = exc.__traceback__.tb_next
-                else:
-                    logger.warning('The first frame is not the current frame.')
-
+            _remove_frame(exc=exc, frame=inspect.currentframe())
             result.exc = exc
 
         hook.hook.finalize_run_result(run_result=result)
 
         return result
+
+
+def _remove_frame(exc: BaseException, frame: Optional[FrameType]) -> None:
+    if exc.__traceback__ and frame and exc.__traceback__.tb_frame is frame:
+        exc.__traceback__ = exc.__traceback__.tb_next
