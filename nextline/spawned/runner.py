020b82f3ecae5255862062df3095db7ea6bee876
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-21 11:38:24 -0400

    extract _compile_and_run()

--- nextline/spawned/runner.py
+++ nextline/spawned/runner.py
@@ -2,6 +2,8 @@ import inspect
 from types import FrameType
 from typing import Optional
 
+from apluggy import PluginManager
+
 from .call import sys_trace
 from .plugin import build_hook
 from .types import QueueIn, QueueOut, RunArg, RunResult
@@ -13,26 +15,32 @@ def run(run_arg: RunArg, queue_in: QueueIn, queue_out: QueueOut) -> RunResult:
 
     with hook.with_.context():
 
-        result = RunResult(ret=None, exc=None)
-
-        try:
-            func = hook.hook.compose_callable()
-        except BaseException as exc:
-            result.exc = exc
-            return result
-
-        trace_func = hook.hook.create_trace_func()
-
-        try:
-            with sys_trace(trace_func=trace_func):
-                result.ret = func()
-        except BaseException as exc:
-            _remove_frame(exc=exc, frame=inspect.currentframe())
-            result.exc = exc
+        result = _compile_and_run(hook=hook)
 
         hook.hook.finalize_run_result(run_result=result)
 
         return result
+
+
+def _compile_and_run(hook: PluginManager) -> RunResult:
+
+    try:
+        func = hook.hook.compose_callable()
+    except BaseException as exc:
+        _remove_frame(exc=exc, frame=inspect.currentframe())
+        return RunResult(ret=None, exc=exc)
+
+    trace_func = hook.hook.create_trace_func()
+
+    result = RunResult(ret=None, exc=None)
+    try:
+        with sys_trace(trace_func=trace_func):
+            result.ret = func()
+    except BaseException as exc:
+        _remove_frame(exc=exc, frame=inspect.currentframe())
+        result.exc = exc
+
+    return result
 
 
 def _remove_frame(exc: BaseException, frame: Optional[FrameType]) -> None:
