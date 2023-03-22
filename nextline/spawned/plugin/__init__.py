0b376f5ee5968dbfa097f6be8493c283490275b9
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-22 09:45:37 -0400

    merge hook.py into __init__.py

--- nextline/spawned/plugin/__init__.py
+++ nextline/spawned/plugin/__init__.py
@@ -1,4 +1,34 @@
 __all__ = ['Hook']
 
 
-from .hook import Hook
+from logging import getLogger
+
+from apluggy import PluginManager
+
+from nextline.spawned.types import QueueIn, QueueOut, RunArg
+
+from . import plugins, skip, spec
+
+
+def Hook(run_arg: RunArg, queue_in: QueueIn, queue_out: QueueOut) -> PluginManager:
+    '''Return a plugin manager with the plugins registered.'''
+
+    hook = PluginManager(spec.PROJECT_NAME)
+    hook.add_hookspecs(spec)
+
+    plugins.register(hook)
+
+    logger = getLogger(__name__)
+    plugin_names = (f'{n!r}' for n, p in hook.list_name_plugin() if p)
+    msg = f'Loaded plugins: {",".join(plugin_names)}.'
+    logger.info(msg)
+
+    hook.hook.init(
+        hook=hook,
+        run_arg=run_arg,
+        modules_to_skip=skip.MODULES_TO_SKIP,
+        queue_in=queue_in,
+        queue_out=queue_out,
+    )
+
+    return hook
