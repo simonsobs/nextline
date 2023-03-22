3bc9d2363ac8d3c2d45f05c79b3c4b968032b345
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-22 09:34:19 -0400

    clean code

--- nextline/spawned/plugin/plugins/__init__.py
+++ nextline/spawned/plugin/plugins/__init__.py
@@ -1,20 +1,7 @@
-__all__ = [
-    'CallableComposer',
-    'TaskAndThreadKeeper',
-    'TaskOrThreadToTraceMapper',
-    'FilerByModule',
-    'FilterByModuleName',
-    'FilterLambda',
-    'GlobalTraceFunc',
-    'TraceFuncCreator',
-    'LocalTraceFunc',
-    'TraceCallHandler',
-    'PdbInstanceFactory',
-    'Prompt',
-    'PeekStdout',
-    'Repeater',
-]
+__all__ = ['register']
 
+
+from apluggy import PluginManager
 
 from .compose import CallableComposer
 from .concurrency import TaskAndThreadKeeper, TaskOrThreadToTraceMapper
@@ -24,3 +11,20 @@ from .local_ import LocalTraceFunc, TraceCallHandler
 from .pdb_ import PdbInstanceFactory, Prompt
 from .peek import PeekStdout
 from .repeat import Repeater
+
+
+def register(hook: PluginManager) -> None:
+    hook.register(Repeater)
+    hook.register(PeekStdout)
+    hook.register(Prompt)
+    hook.register(PdbInstanceFactory)
+    hook.register(TraceCallHandler)
+    hook.register(LocalTraceFunc)
+    hook.register(TaskOrThreadToTraceMapper)
+    hook.register(TaskAndThreadKeeper)
+    hook.register(FilerByModule)
+    hook.register(FilterLambda)
+    hook.register(FilterByModuleName)
+    hook.register(GlobalTraceFunc)
+    hook.register(TraceFuncCreator)
+    hook.register(CallableComposer)
