94ca0d30baec1d39912e822e80ce90675ee64e41
Tai Sakuma <tai.sakuma@gmail.com>
2023-03-22 14:44:15 -0400

    update a test

--- tests/main/test_prompts.py
+++ tests/main/test_prompts.py
@@ -1,13 +1,15 @@
+import time
+
 from nextline import Nextline
 
-SOURCE = '''
-import time
-time.sleep(0.001)
-'''.strip()
+
+def func():
+
+    time.sleep(0.001)
 
 
 async def test_one() -> None:
-    async with Nextline(SOURCE) as nextline:
+    async with Nextline(func) as nextline:
         async with nextline.run_session():
             async for prompt in nextline.prompts():
                 nextline.send_pdb_command('next', prompt.prompt_no, prompt.trace_no)
