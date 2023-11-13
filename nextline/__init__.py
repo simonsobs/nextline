6161eb526d9ae6c425fbf137053ecef0a69cb575
Tai Sakuma <tai.sakuma@gmail.com>
2023-11-13 10:00:55 -0500

    Export Statement

--- nextline/__init__.py
+++ nextline/__init__.py
@@ -2,9 +2,11 @@ __all__ = [
     '__version__',
     'disable_trace',
     'Nextline',
+    'Statement',
 ]
 
 from nextline.__about__ import __version__
 
 from .disable import disable_trace
 from .main import Nextline
+from .types import Statement
