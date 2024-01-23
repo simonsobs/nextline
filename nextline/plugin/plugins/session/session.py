d0cdb6576b6fb792b8182c40c150419c2903b2a3
Tai Sakuma <tai.sakuma@gmail.com>
2024-01-22 20:10:42 -0500

    Merge spawn.py into session.py

--- nextline/plugin/plugins/session/session.py
+++ nextline/plugin/plugins/session/session.py
@@ -1,15 +1,19 @@
-from collections.abc import AsyncIterator
+import asyncio
+import contextlib
+import multiprocessing as mp
+import time
+from collections.abc import AsyncIterator, Callable
+from functools import partial
 from logging import getLogger
-from typing import Any, Optional
+from typing import Any, Optional, cast
 
 import apluggy
 from tblib import pickling_support
 
+from nextline import spawned
 from nextline.plugin.spec import Context, hookimpl
-from nextline.spawned import Command, RunResult
-from nextline.utils import ExitedProcess
-
-from .spawn import run_session
+from nextline.spawned import Command, QueueIn, QueueOut, RunResult
+from nextline.utils import ExitedProcess, RunningProcess, run_in_process
 
 pickling_support.install()
 
@@ -28,6 +32,56 @@ class RunSession:
             logger.exception(exited.raised)
         self._run_result = exited.returned or RunResult()
         await ahook.on_end_run(context=context, exited_process=exited)
+
+
+@contextlib.asynccontextmanager
+async def run_session(context: Context) -> AsyncIterator[RunningProcess[RunResult]]:
+    assert context.run_arg
+    mp_context = mp.get_context('spawn')
+    queue_in = cast(QueueIn, mp_context.Queue())
+    queue_out = cast(QueueOut, mp_context.Queue())
+    context.send_command = SendCommand(queue_in)
+    func = partial(spawned.main, context.run_arg)
+    initializer = partial(spawned.set_queues, queue_in, queue_out)
+    async with relay_events(context, queue_out):
+        context.running_process = await run_in_process(
+            func, mp_context=mp_context, initializer=initializer, collect_logging=True
+        )
+        yield context.running_process
+
+
+def SendCommand(queue_in: QueueIn) -> Callable[[Command], None]:
+    def _send_command(command: Command) -> None:
+        logger = getLogger(__name__)
+        logger.debug(f'send_pdb_command({command!r}')
+        queue_in.put(command)
+
+    return _send_command
+
+
+@contextlib.asynccontextmanager
+async def relay_events(context: Context, queue: QueueOut) -> AsyncIterator[None]:
+    '''Call the hook `on_event_in_process()` on events emitted in the spawned process.'''
+    logger = getLogger(__name__)
+
+    async def _monitor() -> None:
+        while (event := await asyncio.to_thread(queue.get)) is not None:
+            logger.debug(f'event: {event!r}')
+            await context.hook.ahook.on_event_in_process(context=context, event=event)
+
+    task = asyncio.create_task(_monitor())
+    try:
+        yield
+    finally:
+        up_to = 0.05
+        start = time.process_time()
+        while not queue.empty():
+            await asyncio.sleep(0)
+            if time.process_time() - start > up_to:
+                logger.warning(f'Timeout. the queue is not empty: {queue!r}')
+                break
+        await asyncio.to_thread(queue.put, None)  # type: ignore
+        await task
 
 
 class Signal:
