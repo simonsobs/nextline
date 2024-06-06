49f80e6ce9be27986b4db980fffeec78c8ae08c9
Tai Sakuma <tai.sakuma@gmail.com>
2024-06-06 13:59:18 -0400

    Correct type hints

--- tests/test_count.py
+++ tests/test_count.py
@@ -1,3 +1,6 @@
+from collections.abc import Callable
+from typing import Protocol, TypeAlias
+
 from hypothesis import given
 from hypothesis import strategies as st
 
@@ -11,7 +14,17 @@ from nextline.count import (
 from nextline.types import PromptNo, RunNo, TaskNo, ThreadNo, TraceNo
 from tests.test_utils.st import st_none_or
 
-COUNT_COUNTER_PAIRS = [
+Counter: TypeAlias = Callable[[], int]
+
+
+class CounterFactory(Protocol):
+    def __call__(self, start: int = 1) -> Counter:
+        ...
+
+
+CounterCountPair: TypeAlias = tuple[CounterFactory, type[int]]
+
+COUNTER_COUNT_PAIRS: list[CounterCountPair] = [
     (RunNoCounter, RunNo),
     (TraceNoCounter, TraceNo),
     (ThreadNoCounter, ThreadNo),
@@ -21,16 +34,16 @@ COUNT_COUNTER_PAIRS = [
 
 
 @given(
-    pair=st.sampled_from(COUNT_COUNTER_PAIRS),
+    pair=st.sampled_from(COUNTER_COUNT_PAIRS),
     start=st_none_or(st.integers()),
     n=st.integers(min_value=1, max_value=10),
 )
-def test_counter(pair, start: int | None, n: int) -> None:
-    counter, count = pair
+def test_counter(pair: CounterCountPair, start: int | None, n: int) -> None:
+    counter_factory, count = pair
     if start is not None:
-        counter = counter(start)
+        counter = counter_factory(start)
     else:
-        counter = counter()
+        counter = counter_factory()
         start = 1
     for i in range(n):
         assert count(start + i) == counter()
