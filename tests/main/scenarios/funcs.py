551e292becb03d72a5defe2e45de5c269a25c620
Tai Sakuma <tai.sakuma@gmail.com>
2024-07-03 11:18:48 -0400

    Add a docstring

--- tests/main/scenarios/funcs.py
+++ tests/main/scenarios/funcs.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import dataclasses
 from collections.abc import Iterable
 from typing import TYPE_CHECKING, TypeVar
@@ -10,6 +8,24 @@ if TYPE_CHECKING:
     _T = TypeVar('_T', bound=DataclassInstance)
 
 
-def replace_with_bool(obj: _T, fields: Iterable[str]) -> _T:
+def replace_with_bool(obj: '_T', fields: Iterable[str]) -> '_T':
+    '''Set the fields of the dataclass instance to True or False.
+
+    This function is used to assert, for example, optional datetime fields are set.
+
+
+    >>> import datetime
+    >>> from typing import Optional
+
+    >>> @dataclasses.dataclass
+    ... class Foo:
+    ...     a: int
+    ...     b: Optional[datetime.datetime] = None
+    ...     c: Optional[datetime.datetime] = None
+    >>> foo = Foo(1, c=datetime.datetime.now())
+    >>> replace_with_bool(foo, ('b', 'c'))
+    Foo(a=1, b=False, c=True)
+
+    '''
     changes = {f: not not getattr(obj, f) for f in fields}
     return dataclasses.replace(obj, **changes)  # type: ignore
