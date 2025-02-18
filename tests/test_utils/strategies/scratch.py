2aefeab3548e782d77d45f655aacdef9aa8b272a
Tai Sakuma <tai.sakuma@gmail.com>
2025-02-18 09:05:21 -0500

    Commit `test_scratch.py`

---
+++ tests/test_utils/strategies/scratch.py
@@ -0,0 +1,9 @@
+
+
+import time
+
+
+def func_0001():
+    print('here!')
+    time.sleep(0.001)
+
---
+++ tests/test_utils/strategies/test_scratch.py
@@ -0,0 +1,20 @@
+from black import Mode, format_str
+from hypothesis import given
+from hypothesis import strategies as st
+
+from .st_node import st_thread_node_list
+
+
+@given(data=st.data())
+def test_st_thread_node_list(data: st.DataObject) -> None:
+    threads = data.draw(st_thread_node_list(max_size=5, generate_tasks=False))
+    ic(threads)
+    lines = ['#']
+    lines[len(lines):] = ['def f1():', '    print("f1")']
+    lines[len(lines):] = ['f1()']
+
+    script = '\n'.join(lines)
+    script = format_str(script, mode=Mode())
+    ic(script)
+    exec(script, globals())
+
